22d5f8b25ae381b82bfb5d90e2674573465a6708
Uwe Schmitt <uschmitt@mineway.de>
2013-07-26 11:43:30 +0200

    [FEATURE] added run_appstore_server.sh

--- test_install.sh
+++ test_install.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 #
 # 1) virtualenv
 # 2) pip install -i http://localhost:8080/simple
@@ -6,3 +7,20 @@
 # 4) startup workbench
 
 # wie repository konfigurieren ???? -> umgebungsvariable
+
+echo
+echo 
+rm -rvf ../tmp/tmp.*
+echo
+echo
+TMPDIR=$(mktemp -d --tmpdir=../tmp)
+virtualenv $TMPDIR
+. $TMPDIR/bin/activate
+echo 
+echo
+pip install -i http://localhost:8080/simple emzed.workbench
+pip install -i http://localhost:8080/simple emzed.core
+python -c "import emzed.workbench"
+python -c "import emzed.core"
+
+
