54ce0db63d5179a8afd55641992441ecf965828c
Uwe Schmitt <uschmitt@mineway.de>
2013-08-22 10:10:10 +0200

    [TRAVIS] tried  to fix integration test test_xcms_connector.py

--- integration_tests/test_xcms_connector.py
+++ integration_tests/test_xcms_connector.py
@@ -10,7 +10,10 @@ def test_install_xcms(tmpdir):
     # order of next three lines is important for sucessfull  patching
     from emzed.core.r_connect import RExecutor
     RExecutor._patched_rlibs_folder = tmpdir.strpath
-    from emzed.core.r_connect import *
+    from emzed.core.r_connect import (installXcmsIfNeeded,
+                                       checkIfxcmsIsInstalled,
+                                       lookForXcmsUpgrades,
+                                       doXcmsUpgrade)
 
     assert installXcmsIfNeeded() == 1
     assert checkIfxcmsIsInstalled() == 1
