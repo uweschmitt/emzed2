5ef337f9ec6fafb7064b93c3d2059de64bdc6165
Uwe Schmitt <uschmitt@mineway.de>
2013-08-29 13:39:59 +0200

    [FIX] added missing classes and functions to emzed.gui

--- emzed/gui/__init__.py
+++ emzed/gui/__init__.py
@@ -1,2 +1,12 @@
 from file_dialogs import (askForDirectory, askForSave, askForSingleFile, askForMultipleFiles,
                           chooseConfig)
+
+
+from dialog_builder import (showWarning, showInformation, DialogBuilder, RunJobButton,
+                            WorkflowFrontend)
+
+del file_dialogs
+
+del dialog_builder
+
+
