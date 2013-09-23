df7ff4a0c8601b9c5543d1cadeafb49514ddb85b
Uwe Schmitt <uschmitt@mineway.de>
2013-09-23 15:48:28 +0200

    [FEATURE] removed import of emzed from setup.py

--- setup.py
+++ setup.py
@@ -9,12 +9,16 @@ def patched(self):
                 )
 distutils.config.PyPIRCCommand._read_pypirc = patched
 
+
+version_str = "2.0.1"
+
+
+version_tuple = tuple(map(int, version_str.split(".")))
+with open("emzed/version.py", "w") as fp:
+    fp.write("version = %r\n" % (version_tuple,))
+
+
 from setuptools import setup
-
-import emzed
-version_str = ".".join(map(str, emzed.__version__))
-
-
 setup(name="emzed",
       packages=[ "emzed"],
       version=version_str,
@@ -27,6 +31,17 @@ setup(name="emzed",
           "emzed.core.r_connect": [ "*.txt"]
           },
       zip_safe=False,
-      install_requires = [ "emzed_optimizations"]
+      install_requires = [ "emzed_optimizations", 
+          "guidata>=1.6.0",
+          "guiqwt>=2.3.1",
+          "requests",
+          "numpy",
+          "scipy",
+          "matplotlib",
+          "sphinx",
+          "pyopenms",
+          "spyder==2.1.13",
+          "html2text",
+          ]
 
      )
