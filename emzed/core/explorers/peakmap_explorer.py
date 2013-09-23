91e2a0401c23ee45c043e8ecc164cf29d4f834de
Uwe Schmitt <uschmitt@mineway.de>
2013-09-23 17:32:59 +0200

    [FEAURE] fast sampling mz peaks introduced in peakmap explorer

--- emzed/core/explorers/peakmap_explorer.py
+++ emzed/core/explorers/peakmap_explorer.py
@@ -23,7 +23,7 @@ from guiqwt.signals import (SIG_MOVE, SIG_START_TRACKING, SIG_STOP_NOT_MOVING, S
                             SIG_PLOT_AXIS_CHANGED, )
 from guiqwt.tools import SelectTool, InteractiveTool
 
-from emzed_optimizations.sample import sample_image
+from emzed_optimizations.sample import sample_image, sample_peaks
 
 from plotting_widgets import MzPlotter
 
@@ -979,7 +979,7 @@ class PeakMapExplorer(QDialog):
         p = self.chromatogram_widget.plot
         p.plot_chromatogram(rts, chroma)
 
-        peaks = self.peakmap.extract(rtmin, rtmax, mzmin, mzmax).all_peaks(msLevel=1)
+        peaks = sample_peaks(self.peakmap, rtmin, rtmax, mzmin, mzmax, 3000)
         self.mz_plotter.plot_peaks(peaks)
         self.mz_plotter.widget.plot.reset_x_limits()
         self.mz_plotter.widget.plot.reset_y_limits()
