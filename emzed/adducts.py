uweschmitt/emzed2

commit 7f42520ab07472a47fad736b4a064294be84834a

[FEATURE] added new adduct [M+KCl-H]

emzed/adducts.py
@@ -25,6 +25,8 @@ _all_adducts=[("M+H"      , _mass.p                    , 1),
               ("M+Na-2H"  , -2*_mass.p + _mass.Na     , -1),
               ("M+Cl"     , _mass.Cl + _mass.e  , -1),
               ("M+K-2H"   , -2*_mass.p + _mass.K      , -1),
+              ("M+K-2H"   , -2*_mass.p + _mass.K      , -1),
+              ("M+KCl-H"  , -1*_mass.p + _mass.K + _mass.C, -1),
               ("M+FA-H"   , _mass.of("H2CO2")-_mass.p , -1),
               ("M-2H"     , -2*_mass.p                 , -2),
               ("M-3H"     , -3*_mass.p                 , -3),

commit 74d25f146c9878038fd9a5176fc6490d58a06017

[FIX] fixed two bugs in adducts.py introduced in commit 7f42520a

emzed/adducts.py
@@ -25,8 +25,7 @@ _all_adducts=[("M+H"      , _mass.p                    , 1),
               ("M+Na-2H"  , -2*_mass.p + _mass.Na     , -1),
               ("M+Cl"     , _mass.Cl + _mass.e  , -1),
               ("M+K-2H"   , -2*_mass.p + _mass.K      , -1),
-              ("M+K-2H"   , -2*_mass.p + _mass.K      , -1),
-              ("M+KCl-H"  , -1*_mass.p + _mass.K + _mass.C, -1),
+              ("M+KCl-H"  , -1*_mass.p + _mass.K + _mass.Cl, -1),
               ("M+FA-H"   , _mass.of("H2CO2")-_mass.p , -1),
               ("M-2H"     , -2*_mass.p                 , -2),
               ("M-3H"     , -3*_mass.p                 , -3),

tests/test_chemistry_helpers.py
@@ -18,11 +18,11 @@ def testAccessAndConsistency():
 
 
 def testAdducts():
-    assert len(adducts.all) == 30, len(adducts.all)
+    assert len(adducts.all) == 29, len(adducts.all)
     # test subgroups
     assert len(adducts.positive) == 17, len(adducts.positive)
     assert len(adducts.neutral) == 1, len(adducts.neutral)
-    assert len(adducts.negative) == 30 - 17 - 1, len(adducts.negative)
+    assert len(adducts.negative) == 29 - 17 - 1, len(adducts.negative)
     assert len(adducts.single_charged) > 0
     assert len(adducts.double_charged) > 0
     assert len(adducts.triple_charged) > 0