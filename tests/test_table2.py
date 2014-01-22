uweschmitt/emzed2

commit dcccb7f8ee0df80d9e99532164aa7dc73a6c7a72

[FEATURE] grouped aggregate expressions

emzed/core/data_types/expressions.py
@@ -1,7 +1,7 @@
-import pdb
 import numpy as np
 import re
 import col_types
+import collections
 
 
 __doc__ = """
@@ -146,6 +146,8 @@ def cleanup(type_):
                 return int
             if np.floating in mro:
                 return float
+        if basestring in mro:
+            return str
     return type_
 
 
@@ -465,6 +467,17 @@ class BaseExpression(object):
         """
         return AggregateExpression(self, lambda v: np.mean(v).tolist(), "mean(%s)", float)
 
+
+    @property
+    def median(self):
+        """
+        This is an **aggregation expression** which evaluates an
+        expression to its mean.
+
+        Example: ``tab.area.mean``
+        """
+        return AggregateExpression(self, lambda v: np.median(v).tolist(), "mean(%s)", float)
+
     @property
     def std(self):
         """
@@ -483,7 +496,7 @@ class BaseExpression(object):
         instead.
         """
         return AggregateExpression(self, lambda v: len(v), "len(%s)",
-                                   int, ignoreNone=False, default_empty=0)
+                                   int, ignore_none=False, default_empty=0)
 
     @property
     def count(self):
@@ -494,7 +507,7 @@ class BaseExpression(object):
         Example:: ``tab.id.len``
         """
         return AggregateExpression(self, lambda v: len(v), "count(%s)",
-                                   int, ignoreNone=False, default_empty=0)
+                                   int, ignore_none=False, default_empty=0)
 
     @property
     def countNone(self):
@@ -504,7 +517,7 @@ class BaseExpression(object):
         """
         return AggregateExpression(self,
                                    lambda v: sum(1 for vi in v if vi is None),
-                                   "countNone(%s)", int, ignoreNone=False,
+                                   "countNone(%s)", int, ignore_none=False,
                                    default_empty=0)
 
     @property
@@ -515,7 +528,7 @@ class BaseExpression(object):
         """
         return AggregateExpression(self,
                                    lambda v: sum(1 for vi in v if vi is not None),
-                                   "countNotNone(%s)", int, ignoreNone=False,
+                                   "countNotNone(%s)", int, ignore_none=False,
                                    default_empty=0)
 
     @property
@@ -526,7 +539,7 @@ class BaseExpression(object):
         value.
         """
         return AggregateExpression(self, lambda v: int(None in v),
-                                   "hasNone(%s)", bool, ignoreNone=False,
+                                   "hasNone(%s)", bool, ignore_none=False,
                                    default_empty=0)
 
     @property
@@ -549,7 +562,7 @@ class BaseExpression(object):
                                 % (self, sorted(diff)))
             return diff.pop()
         return AggregateExpression(self, select, "uniqueNotNone(%s)",
-                                   None, ignoreNone=False)
+                                   None, ignore_none=False)
 
     @property
     def values(self):
@@ -824,28 +837,78 @@ class BinaryExpression(BaseExpression):
             return res, idx, ct
 
         if ll == 1:
-            values = [self.efun(lvals[0], r) for r in rvals]
+            if lvals[0] is None:
+                values = [None] * len(rvals)
+            else:
+                values = [self.efun(lvals[0], r) if r is not None else None for r in rvals]
         elif lr == 1:
-            values = [self.efun(l, rvals[0]) for l in lvals]
+            if rvals[0] is None:
+                values = [None] * len(lvals)
+            else:
+                values = [self.efun(l, rvals[0]) if l is not None else l for l in lvals]
         else:
-            values = [self.efun(l, r) for (l, r) in zip(lvals, rvals)]
+            values = [self.efun(l, r) if l is not None and r is not None else None
+                      for (l, r) in zip(lvals, rvals)]
 
         return container(ct)(values), None, ct
 
 
+class GroupedAggregateExpression(BaseExpression):
+
+    def __init__(self, left, efun, default_empty, ignore_none,  group_by_column):
+        self.left = left
+        self.efun = efun
+        self.default_empty = default_empty
+        self.ignore_none = ignore_none
+        self.group_by_column = group_by_column
+
+    def _eval(self, ctx=None):
+        child_values, __, child_type = saveeval(self.left, ctx)
+        group_values, __, group_type = saveeval(self.group_by_column, ctx)
+
+        grouped_values = collections.defaultdict(list)
+        for (g, v) in zip(group_values, child_values):
+            grouped_values[g].append(v)
+
+        aggregated_values = dict()
+        for g, values in grouped_values.items():
+            if self.ignore_none:
+                values = [v for v in values if v is not None]
+            if not len(values):
+                aggregated_values[g] = self.default_empty
+            else:
+                type_ = common_type_for(values)
+                values = np.array(values)
+                aggregated_values[g] = self.efun(values)
+
+        result = [aggregated_values[g] for g in group_values]
+        type_ = common_type_for(result)
+        result = container(type_)(result)
+        type_ = cleanup(type_)
+        return np.array(result), None, type_
+
+
 class AggregateExpression(BaseExpression):
 
-    def __init__(self,
-                 left, efun, funname, restype,
-                 ignoreNone=True, default_empty=None):
+    def __init__(self, left, efun, funname, restype, ignore_none=True, default_empty=None):
         if not isinstance(left, BaseExpression):
             left = Value(left)
         self.left = left
         self.efun = efun
         self.funname = funname
         self.restype = restype
-        self.ignoreNone = ignoreNone
+        self.ignore_none = ignore_none
         self.default_empty = default_empty
+
+    def group_by(self, group_by_column):
+        return GroupedAggregateExpression(self.left, self.efun, self.default_empty,
+                self.ignore_none, group_by_column)
+
+    def __call__(self):
+        values, _, type_ = self._eval()
+        if len(values):
+            return values[0]
+        return self.default_empty
 
     def _eval(self, ctx=None):
         vals, _, type_ = saveeval(self.left, ctx)
@@ -854,29 +917,23 @@ class AggregateExpression(BaseExpression):
 
         if type_ in _basic_num_types:
             vals = vals.tolist()
-        if self.ignoreNone:
+        if self.ignore_none:
             vals = [v for v in vals if v is not None]
+
         if len(vals):
-            result = [self.efun(vals)]
-            result = container(type(result[0]))(result)
+            agg_value = self.efun(vals)
+            result = container(type(agg_value))([agg_value])
             type_ = cleanup(type(result[0]))
-            return result, self.default_empty, type_
-        # only nones !
+            return result,  None, type_
+
         if type_ in _basic_num_types:
             return np.array((self.default_empty,),), None, type_
+
         return [self.default_empty], None, type_
 
     def __str__(self):
         return self.funname % self.left
 
-    def __call__(self):
-        values = self.values
-        if len(values):
-            v = values[0]
-            if v is not None and self.restype is not None:
-                return self.restype(v)
-            return v
-        return self.default_empty
 
     def _evalsize(self):
         return 1

tests/test_table2.py
@@ -133,7 +133,7 @@ def testApplyUfun():
 def testNonBoolean():
     t = emzed.utils.toTable("a", [])
     try:
-        not t.a
+        not t.a  # this was a common mistake: ~t.a is the correct way to express negation
     except:
         pass
     else:
@@ -286,6 +286,7 @@ def testAggregateOperation():
     t = t.aggregate(t.b.countNone, "countNone", groupBy="a")
     t = t.aggregate(t.b.count, "count", groupBy="a")
     t = t.aggregate(t.b.count - t.b.countNotNone, "countNone2", groupBy="a")
+    print t.b.std.values
     t = t.aggregate(t.b.std * t.b.std, "var", groupBy="a")
     t = t.aggregate(t.b.mean, "mean", groupBy="a")
     t._print(w=8)
@@ -552,3 +553,90 @@ def test_collapse():
     assert len(subs[1]) == 1
     assert subs[2].getColNames() == ["id", "a", "b"]
     assert len(subs[2]) == 1
+
+
+def test_missing_values_binary_expressions():
+
+    # column x has type None, this is a special case which failed before
+    t = emzed.utils.toTable("x", [None])
+    assert (t.x + t.x).values == [None]
+    assert (t.x * t.x).values == [None]
+    assert (t.x - t.x).values == [None]
+    assert (t.x / t.x).values == [None]
+
+    t.addColumn("y", [1])
+    assert (t.x + t.y).values == [None]
+    assert (t.x * t.y).values == [None]
+    assert (t.x - t.y).values == [None]
+    assert (t.x / t.y).values == [None]
+
+    assert (t.y + t.x).values == [None]
+    assert (t.y * t.x).values == [None]
+    assert (t.y - t.x).values == [None]
+    assert (t.y / t.x).values == [None]
+
+
+def test_grouped_aggregate_expressions():
+
+    # without missing values #########################################
+
+    # int types aggregated
+    t = emzed.utils.toTable("group", [1, 1, 2])
+    t.addColumn("values", [1, 2, 3])
+    t.addColumn("grouped_min", t.values.min.group_by(t.group))
+    assert t.grouped_min.values == [1, 1, 3]
+    t.print_()
+
+    # float types aggregated
+    t = emzed.utils.toTable("group", [1, 1, 2])
+    t.addColumn("values", [1, 2, 3])
+    t.addColumn("grouped_mean", t.values.mean.group_by(t.group))
+    assert t.grouped_mean.values == [1.5, 1.5, 3]
+    t.print_()
+
+    # str types aggregated
+    t = emzed.utils.toTable("group", [1, 1, 2])
+    t.addColumn("values", ["1", "2", "3"])
+    t.addColumn("grouped_max", t.values.max.group_by(t.group))
+    assert t.grouped_max.values == ["2", "2", "3"]
+    t.print_()
+
+    # with missing values ############################################
+
+    # int types aggregated
+    t = emzed.utils.toTable("group", [1, 1, 2])
+    t.addColumn("values", [None, 2, None])
+    t.addColumn("grouped_min", t.values.min.group_by(t.group))
+    assert t.grouped_min.values == [2, 2, None]
+    t.print_()
+
+    # float types aggregated
+    t = emzed.utils.toTable("group", [1, 1, 2])
+    t.addColumn("values", [None, 2.0, None])
+    t.addColumn("grouped_min", t.values.min.group_by(t.group))
+    assert t.grouped_min.values == [2.0, 2.0, None]
+    t.print_()
+
+    # str types aggregated
+    t = emzed.utils.toTable("group", [1, 1, 2])
+    t.addColumn("values", [None, "2", None])
+    t.addColumn("grouped_min", t.values.min.group_by(t.group))
+    assert t.grouped_min.values == ["2", "2", None]
+    t.print_()
+
+    # empty columns ##################################################
+
+    # only None values
+    t = emzed.utils.toTable("group", [1, 1, 2])
+    t.addColumn("values", [None, None, None])
+    t.addColumn("grouped_min", t.values.min.group_by(t.group))
+    assert t.grouped_min.values == [None, None, None]
+    t.print_()
+
+    # empty table
+    t = emzed.utils.toTable("group", [])
+    t.addColumn("values", [])
+    t.addColumn("grouped_min", t.values.min.group_by(t.group))
+    assert t.grouped_min.values == []
+    t.print_()
+

tests/test_table.py
@@ -348,7 +348,7 @@ class TestTable(unittest.TestCase):
             assert a[0] in self.expected
             return True # suppress exceptoin
 
-    def testUniqeNotNone(self):
+    def testUniqueNotNone(self):
 
         t = toTable("a", [1,1,None])
         assert t.a.uniqueNotNone() == 1

commit 8a21669e093478c22faf924930faeee165ca39ba

[FIX] fixed regression from dcccb7f

emzed/core/data_types/expressions.py
@@ -1,4 +1,3 @@
-import pdb
 import numpy as np
 import re
 import col_types
@@ -922,7 +921,8 @@ class AggregateExpression(BaseExpression):
     def __call__(self):
         values, _, type_ = self._eval()
         if len(values):
-            return values[0]
+            rv = values[0]
+            return type_(rv)
         return self.default_empty
 
     def _eval(self, ctx=None):

tests/test_table2.py
@@ -621,6 +621,11 @@ def test_own_aggregate_functions():
     t.print_()
 
 
+def test_aggregate_types():
+    t = emzed.utils.toTable("group", [1, 1, 2])
+    assert type(t.group.max()) in (int, long)
+
+
 
 
 