uweschmitt/emzed2

commit 003b1a3b0a53214d5e72e0aa619f9413a298663b

[FIX] fixed/improved initial import handling

emzed/__init__.py
@@ -1,5 +1,27 @@
 # encoding:latin-1
 
+import traceback
+import os
+
+import sitecustomize
+
+_emzed_runs_inside = None
+
+p = traceback.extract_stack()[0][0]
+mod_name = os.path.split(p)[-1]
+
+if mod_name == "emzed.console":
+    _emzed_runs_inside = "emzed.console"
+else:
+    # test if loaded sitecustomize is .../emzed/workbench/sitecustomize.py[c]
+    two_upfolders = os.path.abspath(sitecustomize.__file__).split(os.path.sep)[-3:-1]
+    if two_upfolders == ["emzed", "workbench"]:
+        _emzed_runs_inside = "emzed.workbench"
+    del two_upfolders
+
+del p, mod_name, traceback, os
+
+
 import patches as _patches
 _patches.apply_()
 
@@ -690,5 +712,6 @@ Program, unless a warranty or assumption of liability accompanies a
 copy of the Program in return for a fee.
 
 """
+
 
 

emzed/_tools.py
@@ -1,15 +1,13 @@
 
-def is_started_as_emzed_console():
 
-    if "__emzed_imported_by" in globals():
-        return __emzed_imported_by == "emzed.console"
-    return False
+def runs_inside_emzed_console():
+    import emzed
+    return getattr(emzed, "_emzed_runs_inside", "") == "emzed.console"
 
-def is_started_as_emzed_workbench():
 
-    if "__emzed_imported_by" in globals():
-        return __emzed_imported_by == "emzed.workbench"
-    return False
+def runs_inside_emzed_workbench():
+    import emzed
+    return getattr(emzed, "_emzed_runs_inside", "") == "emzed.workbench"
 
 
 def gui_running():

emzed/config.py
@@ -22,7 +22,7 @@ def _check_first_start():
     # in this case nothing should happen, because we want this actions
     # later in spyders ipython console where is_started_from_cmdline() is False.
     from _tools import *
-    if is_first_start() and is_started_as_emzed_console():
+    if is_first_start() and runs_inside_emzed_console():
         global_config.set_defaults()
         print
         print "loading emzed.config ".ljust(80, ".")
@@ -36,7 +36,7 @@ def _check_first_start():
         print
         print "".ljust(80, ".")
         store()
-    elif is_first_start() and is_started_as_emzed_workbench():
+    elif is_first_start() and runs_inside_emzed_workbench():
         if gui_running():
             aborted = edit(reset_to_defaults=True)
             if not aborted:

emzed/console.py
@@ -3,6 +3,5 @@ def main():
     from emzed.workbench.install import install_emzed
     user_ns = dict()
     install_emzed(user_ns)
-    user_ns["__emzed_imported_by"] = "emzed.console"
     shell = IPython.Shell.IPShellQt4(user_ns=user_ns)
     shell.mainloop()

emzed/core/data_bases/pubchem_db.py
@@ -213,6 +213,7 @@ class PubChemDB(object):
                 callback(i, len(newIds))
             except BaseException, e:
                 if e != "aborted":
+                    self.store()  # in case of bad internet connection save immediate result
                     raise
         try:
             self.table.dropColumns("url")

emzed/core/update_handling.py
@@ -180,7 +180,7 @@ class Updater(object):
         except BaseException, e:
             return None, e.message
         try:
-            is_newer  = self.impl.check_for_newer_version_on_exchange_folder(exchange_folder)
+            is_newer = self.impl.check_for_newer_version_on_exchange_folder(exchange_folder)
         except:
             return False, None
         return is_newer, None
@@ -273,7 +273,7 @@ class EmzedUpdateImpl(AbstractUpdaterImpl):
         pass
 
     def check_for_newer_version_on_exchange_folder(self, exchange_folder):
-        return None, ""
+        return None
 
     def update_from_exchange_folder(self, exchange_folder):
         pass

emzed/updaters.py
@@ -71,7 +71,7 @@ def _print_first_start_info():
 #
 # in this case nothing should happen, because we want this actions
 # later in spyders ipython console where is_started_from_cmdline() is False.
-if _tools.is_started_as_emzed_console() and _tools.is_first_start():
+if _tools.runs_inside_emzed_console() and _tools.is_first_start():
     _print_first_start_info()
 
 

emzed/workbench/main.py
@@ -64,7 +64,7 @@ if is_module_installed('IPython.frontend.qt', '>=0.12'):
 
 #EMZEDADDON
 here = os.path.dirname(os.path.abspath(__file__))
-#os.environ["EMZED_HOME"] = here
+
 import spyderlib
 os.environ["SPYDER_PARENT_DIR"] = os.path.abspath(os.path.join(spyderlib.__file__, "../.."))
 print os.environ["SPYDER_PARENT_DIR"]

commit 622bf608c65a56eaeb5e617d6a7c0df1f5f128f1

[FIX] fixed regression on win from commit 003b1a3

emzed/__init__.py
@@ -3,23 +3,29 @@
 import traceback
 import os
 
-import sitecustomize
+try:
+    import sitecustomize
 
-_emzed_runs_inside = None
+except:
+    pass
 
-p = traceback.extract_stack()[0][0]
-mod_name = os.path.split(p)[-1]
-
-if mod_name == "emzed.console":
-    _emzed_runs_inside = "emzed.console"
 else:
-    # test if loaded sitecustomize is .../emzed/workbench/sitecustomize.py[c]
-    two_upfolders = os.path.abspath(sitecustomize.__file__).split(os.path.sep)[-3:-1]
-    if two_upfolders == ["emzed", "workbench"]:
-        _emzed_runs_inside = "emzed.workbench"
-    del two_upfolders
 
-del p, mod_name, traceback, os
+    _emzed_runs_inside = None
+
+    p = traceback.extract_stack()[0][0]
+    mod_name = os.path.split(p)[-1]
+
+    if mod_name == "emzed.console":
+        _emzed_runs_inside = "emzed.console"
+    else:
+        # test if loaded sitecustomize is .../emzed/workbench/sitecustomize.py[c]
+        two_upfolders = os.path.abspath(sitecustomize.__file__).split(os.path.sep)[-3:-1]
+        if two_upfolders == ["emzed", "workbench"]:
+            _emzed_runs_inside = "emzed.workbench"
+        del two_upfolders
+
+    del p, mod_name, traceback, os
 
 
 import patches as _patches