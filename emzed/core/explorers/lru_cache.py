uweschmitt/emzed2

commit 27cdf3d6d89e17d05f51f7289d2a33d0d781ac6a

[PERFORMANCE] improved performance of peakmap plotting

emzed/core/explorers/lru_cache.py
@@ -1,0 +1,32 @@
+import collections
+import functools
+
+def lru_cache(maxsize=100):
+    '''Least-recently-used cache decorator.
+
+    Arguments to the cached function must be hashable.
+    Cache performance statistics stored in f.hits and f.misses.
+    http://en.wikipedia.org/wiki/Cache_algorithms#Least_Recently_Used
+
+    '''
+    def decorating_function(user_function):
+        cache = collections.OrderedDict()    # order: least recent to most recent
+
+        @functools.wraps(user_function)
+        def wrapper(*args, **kwds):
+            key = args
+            if kwds:
+                key += tuple(sorted(kwds.items()))
+            try:
+                result = cache.pop(key)
+                wrapper.hits += 1
+            except KeyError:
+                result = user_function(*args, **kwds)
+                wrapper.misses += 2
+                if len(cache) >= maxsize:
+                    cache.popitem(0)    # purge least recently used cache entry
+            cache[key] = result         # record recent use of this key
+            return result
+        wrapper.hits = wrapper.misses = 0
+        return wrapper
+    return decorating_function

commit 0584c7953de15354d6a5e6aac5235813f1e02004

[FIX] fixed bug introduced in 27cdf3d

emzed/core/explorers/lru_cache.py
@@ -28,5 +28,7 @@ def lru_cache(maxsize=100):
             cache[key] = result         # record recent use of this key
             return result
         wrapper.hits = wrapper.misses = 0
+        wrapper.cache = cache
+        wrapper.invalidate_cache = lambda: cache.clear()
         return wrapper
     return decorating_function

emzed/core/explorers/peakmap_explorer.py
@@ -93,14 +93,6 @@ class PeakMapImageBase(object):
     def get_peakmap_bounds(self):
         return self.rtmin, self.rtmax, self.mzmin, self.mzmax
 
-    def set_imin(self, imin):
-        self.imin = imin
-
-    def set_imax(self, imax):
-        self.imax = imax
-
-    def set_gamma(self, gamma):
-        self.gamma = gamma
 
     def get_gamma(self):
         return self.gamma
@@ -108,7 +100,24 @@ class PeakMapImageBase(object):
     def get_total_imax(self):
         return self.total_imax
 
+    def set_imin(self, imin):
+        if self.imin != imin:
+            self.compute_image.invalidate_cache()
+        self.imin = imin
+
+    def set_imax(self, imax):
+        if self.imax != imax:
+            self.compute_image.invalidate_cache()
+        self.imax = imax
+
+    def set_gamma(self, gamma):
+        if self.gamma != gamma:
+            self.compute_image.invalidate_cache()
+        self.gamma = gamma
+
     def set_logarithmic_scale(self, is_log):
+        if self.is_log != is_log:
+            self.compute_image.invalidate_cache()
         self.is_log = is_log
 
     @lru_cache(maxsize=100)