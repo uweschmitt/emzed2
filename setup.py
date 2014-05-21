5bec19f3f22c2f2a11e1dc1a280e800e5c88c2a6
uweschmitt <uwe.schmitt@id.ethz.ch>
2014-05-21 11:36:31 +0200

    [RELEASE] v 2.2.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 # no import emzed here, causes trouble when installing on win, as missing packages
 # are needed when importing emzed
-version_str = "2.2.3"
+version_str = "2.2.4"
 
 
 setup(name="emzed",
