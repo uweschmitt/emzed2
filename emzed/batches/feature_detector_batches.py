1c5eea78fe3cada6fac31d4a653b4173c3804c6e
Uwe Schmitt <uwe.schmitt@id.ethz.ch>
2014-07-11 15:29:42 +0200

    [DOCS] updated doc string for ff batches

--- emzed/batches/feature_detector_batches.py
+++ emzed/batches/feature_detector_batches.py
@@ -32,50 +32,50 @@ class FD(batch_runner.BatchRunner):
 def runCentwave(pattern=None, destination=None, configid="std", **params):
 
     """
-         runs centwave algorithm from xcms in batch mode.
-         input files are map files (mzXML, mxML, mzData),
-         ouput files are csv files
+    runs centwave algorithm from xcms in batch mode.
+    input files are map files (mzXML, mxML, mzData),
+    ouput files are csv files
 
-         you can add modifications to the standard parameters, eg ppm,
-         as named arguments.
+    you can add modifications to the standard parameters, eg ppm,
+    as named arguments.
 
-         if you have multiple configs for centwave, you can give an
-         configid as defined in _algorithm_configs.py, or you are asked to choose
-         a config.
+    if you have multiple configs for centwave, you can give an
+    configid as defined in _algorithm_configs.py, or you are asked to choose
+    a config.
 
-         if you have a single config this one is used automatically
+    if you have a single config this one is used automatically
 
-         examples:
+    examples:
 
-              runCentwave():
-                     asks for source files and target directory
-                     asks for config if multiple algorithm_configs are defined
+        runCentwave():
+                asks for source files and target directory
+                asks for config if multiple algorithm_configs are defined
 
-              runCentwave(configid="std", ppm=17)
-                     uses config with id "std", overwrites ppm parameter
-                     with ppm=17.
+        runCentwave(configid="std", ppm=17)
+                uses config with id "std", overwrites ppm parameter
+                with ppm=17.
 
-              runCentwave(ppm=13):
-                     asks for source files and target directory
-                     runs centwave with modified ppm=13 parameter.
+        runCentwave(ppm=13):
+                asks for source files and target directory
+                runs centwave with modified ppm=13 parameter.
 
-              runCentwave(pattern):
-                     looks for map files matching pattern
-                     resulting csv files are stored next to input map file
+        runCentwave(pattern):
+                looks for map files matching pattern
+                resulting csv files are stored next to input map file
 
-              runCentwave(pattern, mzDiff=0.003):
-                     looks for map files matching pattern
-                     resulting csv files are stored next to input map file
-                     runs centwave with modified mzDiff parameter
+        runCentwave(pattern, mzDiff=0.003):
+                looks for map files matching pattern
+                resulting csv files are stored next to input map file
+                runs centwave with modified mzDiff parameter
 
-              runCentwave(pattern, destination):
-                     looks for map files matching pattern
-                     resulting csv files are stored at destination directory
+        runCentwave(pattern, destination):
+                looks for map files matching pattern
+                resulting csv files are stored at destination directory
 
-              runCentwave(pattern, destination, ppm=17, peakwidth=(5,100) ):
-                     looks for map files matching pattern
-                     resulting csv files are stored at destination directory
-                     runs centwave with modified ppm and peakwidth parameters.
+        runCentwave(pattern, destination, ppm=17, peakwidth=(5,100) ):
+                looks for map files matching pattern
+                resulting csv files are stored at destination directory
+                runs centwave with modified ppm and peakwidth parameters.
 
     """
 
