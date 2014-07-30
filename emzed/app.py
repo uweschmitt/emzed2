73f7409c1fb0e6dc9a234bc4cf2267345880358a
uweschmitt <uwe.schmitt@id.ethz.ch>
2014-07-30 18:38:14 +0200

    [FIX] added exception handling when loading emzed.ext or emzed.app

--- emzed/app.py
+++ emzed/app.py
@@ -5,16 +5,23 @@ reload(pkg_resources)  # migh be loaded already and is not up to date
 
 _loaded = []
 
-for ep in pkg_resources.iter_entry_points("emzed_package", name="main"):
-    runner = ep.load()
-    _loaded.append(ep.module_name)
-    name = ep.module_name.split(".")[0]
-    #print "$$$ loaded emzed.app.%s from %s " % (name, runner)
-    exec "%s=runner" % name
-    sys.modules["emzed.app.%s" % name] = runner
-    del ep
-    del runner
-    del name
+for _ep in pkg_resources.iter_entry_points("emzed_package", name="main"):
+    try:
+        _runner = _ep.load()
+    except:
+        del _ep
+        import traceback
+        traceback.print_exc()
+        continue
+
+    _loaded.append(_ep.module_name)
+    _name = _ep.module_name.split(".")[0]
+    #print "$$$ loaded emzed.app.%s from %s " % (_name, _runner)
+    exec "%s=_runner" % _name
+    sys.modules["emzed.app.%s" % _name] = _runner
+    del _ep
+    del _runner
+    del _name
 
 if _loaded:
     print
