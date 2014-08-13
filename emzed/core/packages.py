uweschmitt/emzed2

commit c8a00221b6b83dde5442c07ff298d58e38b75905

[FIX] fixed some error prone overloading of __getattr__

emzed/core/r_connect/r_executor.py
@@ -188,10 +188,11 @@ class RInterpreter(object):
         # attribute 'execute("x <- data")', I think this is driven by the dot in "data.frame"
         if name.startswith("execute("):
             return []
-        value = getattr(self.session, name)
-        if convert_to_table and isinstance(value, pandas.DataFrame):
-            return Table.from_pandas(value)
-        return value
+        if hasattr(self, session):
+            value = getattr(self.session, name)
+            if convert_to_table and isinstance(value, pandas.DataFrame):
+                return Table.from_pandas(value)
+            return value
 
     def __setattr__(self, name, value):
         if isinstance(value, Table):

emzed/core/data_bases/pubchem_db.py
@@ -240,4 +240,6 @@ class PubChemDB(object):
         PubChemDB._instances[path] = self
 
     def __getattr__(self, colName):
-        return getattr(self.table, colName)
+        if hasattr(self, "table"):      # might be undefined eg during unpickling
+            return getattr(self.table, colName)
+        raise AttributeError("attribute table of %r not set" % self)

emzed/core/explorers/peakmap_explorer.py
@@ -1002,7 +1002,8 @@ class PeakMapPlotter(object):
         self.widget.plot.replot()
 
     def __getattr__(self, name):
-        return getattr(self.widget.plot, name)
+        if hasattr(self, "widget"):
+            return getattr(self.widget.plot, name)
 
     def get_plot(self):
         return self.widget.plot

emzed/workbench/sitecustomize.py
@@ -35,7 +35,8 @@ if os.environ.get("COLORIZE_SYS_STDERR", "").lower() == "true"\
             sys.stderr = self
 
         def __getattr__(self, name):
-            return getattr(self.old_stderr, name)
+            if hasattr(self, "old_stderr"):
+                return getattr(self.old_stderr, name)
 
         def write(self, text):
             if os.name == 'nt' and '\n' not in text:

commit b53df84c8033c7bc0f725263ae1c65f0f72b6d5b

[FIX] fixed regression from c8a00221

emzed/core/packages.py
@@ -45,6 +45,8 @@ SETUP_PY_TEMPLATE = """
 
 PKG_NAME = %(pkg_name)r
 
+VERSION = %(version)r
+
 
 ### install package as emzed extension ? #############################################
 #   -> package will appear in emzed.ext namespace after installation
@@ -96,16 +98,6 @@ LICENSE = "http://opensource.org/licenses/GPL-3.0"
 #                                                                                    #
 ######################################################################################
 
-
-from %(pkg_name)s.version import version as VERSION
-
-if APP_MAIN is not None:
-    try:
-        mod_name, fun_name = APP_MAIN.split(":")
-        exec "import %%s as _mod" %% mod_name
-        fun = getattr(_mod, fun_name)
-    except:
-        raise Exception("invalid specification %%r of APP_MAIN" %% APP_MAIN)
 
 VERSION_STRING = "%%s.%%s.%%s" %% VERSION
 
@@ -208,13 +200,9 @@ def _create_package_folder(pkg_folder, pkg_name, version):
 from minimal_module import hello # makes emzed.ext.%s.hello() visible
 
 # DO NOT TOUCH THE FOLLOWING LINE:
-from version import version as __version__
-    """ % pkg_name)
-
-    with open(os.path.join(package_folder, "version.py"), "w") as fp:
-        fp.write("""
-version = %r
-    """ % (version,))
+import pkg_resources
+__version__ = tuple(map(int, pkg_resources.require(__name__)[0].version.split(".")))
+del pkg_resources""" % pkg_name)
 
     with open(os.path.join(package_folder, "app.py"), "w") as fp:
         fp.write("""

emzed/core/r_connect/r_executor.py
@@ -188,7 +188,7 @@ class RInterpreter(object):
         # attribute 'execute("x <- data")', I think this is driven by the dot in "data.frame"
         if name.startswith("execute("):
             return []
-        if hasattr(self, session):
+        if hasattr(self, "session"):
             value = getattr(self.session, name)
             if convert_to_table and isinstance(value, pandas.DataFrame):
                 return Table.from_pandas(value)