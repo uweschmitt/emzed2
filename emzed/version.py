7ee3f0b4881bab4ab983a3204da31fc9b67c3668
uweschmitt <uwe.schmitt@id.ethz.ch>
2014-10-02 18:18:03 +0200

    [FIX] fixed version handling bug when freezing

--- emzed/version.py
+++ emzed/version.py
@@ -1,2 +1,7 @@
-import pkg_resources
-version = tuple(map(int, pkg_resources.require("emzed")[0].version.split(".")))
+import sys
+
+if not getattr(sys, "frozen", False):
+    import pkg_resources
+    version = tuple(map(int, pkg_resources.require("emzed")[0].version.split(".")))
+else:
+    version = (0, 0, 0)
