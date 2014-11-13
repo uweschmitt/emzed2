4067e92f1ad872185fe33430a293133b42c3a645
uweschmitt <uwe.schmitt@id.ethz.ch>
2014-11-13 15:34:44 +0100

    [RELEASE] v 2.5.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 # no import emzed here, causes trouble when installing on win, as missing packages
 # are needed when importing emzed
-version = (2, 5, 1)
+version = (2, 5, 2)
 
 setup(name="emzed",
       packages=find_packages(exclude=["tests", "sandbox"]),
