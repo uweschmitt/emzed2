uweschmitt/emzed2

commit 910eeb6921e405b818a4c4085c90f4f6e24fa0bc

[FIX] added lots of missing files

emzed/gui/dialog_builder.py
@@ -1,0 +1,295 @@
+#encoding: latin-1
+import guidata
+import guidata.dataset.datatypes as dt
+import guidata.dataset.dataitems as di
+
+from guidata.qt.QtGui import QMessageBox
+
+import string
+import locale
+
+# monkey patch following Items, else dt.DataSet.check() raises
+# exceptions. They are assumed to be valid in any case:
+di.BoolItem.check_value = lambda *a, **kw: True
+di.ChoiceItem.check_value = lambda *a, **kw: True
+di.MultipleChoiceItem.check_value = lambda *a, **kw: True
+di.ButtonItem.check_value = lambda *a, **kw: True
+
+def _patched_get(self, instance, klass):
+    if instance is not None:
+         value = getattr(instance, "_" + self._name, self._default)
+         if isinstance(value, unicode):
+             return value.encode(locale.getdefaultlocale()[1])
+         return value
+    return self
+
+di.StringItem.__get__ = _patched_get
+di.TextItem.__get__ = _patched_get
+
+import sys
+
+if sys.platform=="win32":
+
+    def _patched_get_for_pathes(self, instance, klass):
+        if instance is not None:
+            value = getattr(instance, "_" + self._name, self._default)
+            if isinstance(value, unicode):
+                # replace needed for network pathes like "//gram/omics/...."
+                return value.encode(sys.getfilesystemencoding()).replace("/", "\\")
+            return value
+        return self
+
+else:
+    def _patched_get_for_pathes(self, instance, klass):
+        if instance is not None:
+            value = getattr(instance, "_" + self._name, self._default)
+            if isinstance(value, unicode):
+                return value.encode(sys.getfilesystemencoding())
+            return value
+        return self
+
+
+di.FilesOpenItem.__get__ = _patched_get_for_pathes
+di.FileSaveItem.__get__ = _patched_get_for_pathes
+di.FileOpenItem.__get__ = _patched_get_for_pathes
+di.DirectoryItem.__get__ = _patched_get_for_pathes
+
+
+def _translateLabelToFieldname(label):
+    # translate label strings to python vairable names
+    invalid = r"""^°!"\§$%&/()=?´``+*~#'-.:,;<>|@$"""
+    trtable = string.maketrans(invalid, " "*len(invalid))
+    return label.lower().translate(trtable)\
+                .replace("  ", " ")\
+                .replace("  ", " ")\
+                .replace(" ", "_")\
+
+def showWarning(message):
+    """
+    shows a warning dialog with given message
+    """
+
+
+    guidata.qapplication().beep()
+    QMessageBox.warning(None, "Warning", message)
+
+def showInformation(message):
+    """
+    shows a information dialog with given message
+    """
+
+    guidata.qapplication().beep()
+    QMessageBox.information(None, "Information", message)
+
+class DialogBuilder(object):
+
+    # dynamic creation of __doc__
+    _docStrings = []
+    for _itemName, _item in di.__dict__.items():
+        if _itemName.endswith("Item"):
+            _docString = getattr(_item, "__doc__")
+            if _docString is None:
+                _docString = ""
+            _dynamicMethodName = "add"+_itemName[:-4]
+            _docStrings.append(_dynamicMethodName+"(...):\n"+_docString)
+
+    __doc__ = "\n".join(_docStrings)
+
+    def __init__(self, title="Dialog"):
+
+        self.attrnum = 0
+        self.title = title
+        self.items = []
+        self.instructions = []
+        self.fieldNames = []
+        self.buttonCounter = 0
+
+    def __getattr__(self, name):
+        """ dynamically provides methods which start with "add...", eg
+            "addInt(....)".
+
+            If one calls
+
+                   b = Builder()
+                   b.addInt(params)
+
+            then
+
+                   b.addInt
+
+            is a stub function which is constructed and returned some
+            lines below. Then
+
+                   b.addInt(params)
+
+            calls this stub function, which registers the corresponding
+            IntItem with the given params.
+
+        """
+        if name.startswith("add"):
+            try:
+                item = getattr(di, name[3:]+"Item")
+            except:
+                raise AttributeError("%r has no attribute '%s'"\
+                                     % (self, name))
+
+            class Stub(object):
+                def __init__(self, item, outer):
+                    self.item=item
+                    self.outer = outer
+
+                def __call__(self, label, *a, **kw):
+                    #this function registers corresponding subclass of
+                    #    DataItem
+                    fieldName = _translateLabelToFieldname(label)
+                    # check if fieldName is valid in Python:
+                    try:
+                        exec("%s=0" % fieldName) in dict()
+                    except:
+                        raise Exception("converted label %r to field name %r "\
+                                        "which is not allowed in python" \
+                                        % (label, fieldName))
+                    # get DataItem subclass
+                    # construct item
+                    dd = dict ((n,v) for (n,v) in kw.items()
+                                     if n in ["col", "colspan"])
+                    horizontal = kw.get("horizontal")
+                    if horizontal is not None:
+                        del kw["horizontal"]
+                    vertical = kw.get("vertical")
+                    if vertical is not None:
+                        del kw["vertical"]
+                    if "col" in kw:
+                        del kw["col"]
+                    if "colspan" in kw:
+                        del kw["colspan"]
+                    item = self.item(label, *a, **kw)
+                    if dd:
+                        item.set_pos(**dd)
+                    if horizontal:
+                        item.horizontal(horizontal)
+                    if vertical:
+                        item.vertical(vertical)
+
+                    # regiter item and fieldname
+                    self.outer.items.append(item)
+                    self.outer.fieldNames.append(fieldName)
+                    return self.outer
+
+            stub = Stub(item, self)
+
+            # add docstring dynamically
+            item = getattr(di, name[3:]+"Item")
+            docString = getattr(item, "__doc__")
+            docString = "" if  docString is None else docString
+            docString = "-\n\n"+name+"(...):\n"+docString
+            stub.__doc__ = docString
+            return stub
+        raise AttributeError("%r has no attribute '%s'" % (self, name))
+
+    def addInstruction(self, what):
+        self.instructions.append(what)
+        return self
+
+    def addButton(self, label, callback, help=None):
+        """ addButton is not handled by __getattr__, as it need special
+            handling.
+
+            In contrast to the other DateItem subclasses, ButtonItem
+            gets a callback which has to be constructed in a special
+            way, see below.
+        """
+
+        # the signature of 'wrapped' is dictated by the guidata
+        # framework:
+        def wrapped(ds, it, value, parent):
+            # check inputs before callback is executed
+            invalidFields = ds.check()
+            if len(invalidFields):
+                msg = "The following fields are invalid: \n"
+                msg += "\n".join(invalidFields)
+                QMessageBox.warning(parent, "Error", msg)
+                return
+            callback(ds)
+        # register ButtomItem in the same way other DataItem subclass
+        # instances are registered in the "stub" function in
+        # __getattr__:
+        item = di.ButtonItem(label, wrapped, help=help)
+        self.items.append(item)
+        self.fieldNames.append("_button%d" % self.buttonCounter)
+        self.buttonCounter += 1
+        return self
+
+    def show(self):
+        """ opens the constructed dialog.
+
+            In order to d so we construct sublcass of DataSet on the fly.
+
+            the docstring of the class is the title of the dialog,
+            class level attributes are instances of sublcasses of
+            DataItem, eg IntItem.
+
+            For more info see the docs of guidata how those classes
+            are declared to get the wanted dialog.
+
+        """
+        import guidata
+        app = guidata.qapplication()
+
+        # put the class level attributes in a dict
+        attributes = dict(zip(self.fieldNames, self.items))
+        # construct class "Dialog" which is a sublcass of "dt.DataSet"
+        # with the  given attributes:
+        clz = type("Dialog", (dt.DataSet,), attributes)
+        # as said: the docstring is rendered as the dialogues title:
+        clz.__doc__ = self.title+"\n"+"\n".join(self.instructions)
+        # open dialog now !!!
+        instance = clz()
+        if instance.edit() == 0:
+            raise Exception("dialog aborted by user")
+        # return the values a tuple according to the order of the
+        # declared input widgets:
+        result = [getattr(instance, name) for name in self.fieldNames]
+        result = tuple(result)
+        if len(result) == 1:
+            result = result[0]
+        return result
+
+# import guidata DataItems into current namespace
+for _itemName, _item in di.__dict__.items():
+    if _itemName.endswith("Item"):
+        exec ("%s = di.%s" % (_itemName, _itemName))
+
+def RunJobButton(label, method_name=None):
+    item = di.ButtonItem(label, None)
+    item._run_method = method_name
+    return item
+
+
+# patch ok / cancel: never check if needed fields are present ?!
+
+from guidata.dataset.qtwidgets import DataSetEditDialog
+DataSetEditDialog.check = lambda self: True
+
+class WorkflowFrontend(dt.DataSet):
+
+    def __init__(self):
+        import guidata
+        self.app = guidata.qapplication()
+        for item in self._items:
+            if hasattr(item, "_run_method"):
+                name = item._run_method or "run_" + item._name
+                target = getattr(self, name)
+                def inner(ds, it, value, parent, target=target):
+                    invalidFields = ds.check()
+                    if len(invalidFields):
+                        msg = "The following fields are invalid: \n"
+                        msg += "\n".join(invalidFields)
+                        QMessageBox.warning(parent, "Error", msg)
+                        return
+                    target()
+                setattr(self, "_emzed_run_"+name, inner)
+                item.set_prop("display", callback=inner)
+        dt.DataSet.__init__(self)
+
+    show = dt.DataSet.edit

emzed/batches/_BatchRunner.py
@@ -32,7 +32,6 @@ class BatchRunner(object):
             if not files:
                 print "aborted"
                 return
-            import emzed.gui
             destination = emzed.gui.askForDirectory()
             if not destination:
                 print "aborted"

emzed/core/config.py
@@ -1,4 +1,3 @@
-import pdb
 #encoding: latin-1
 
 # keep namespace clean:

emzed/core/data_bases/__init__.py
@@ -1,0 +1,1 @@
+from pubchem_db import PubChemDB

emzed/core/data_bases/pubchem_db.py
@@ -1,0 +1,215 @@
+import requests
+import time, os, sys
+import xml.etree.ElementTree  as etree
+
+from ..chemistry.tools import monoisotopicMass
+from ..data_types import Table
+
+
+def dom_tree_from_bytes(data):
+    if isinstance(data, unicode):
+        return etree.fromstring(data.encode("utf-8"))
+    return etree.fromstring(data)
+
+
+class PubChemDB(object):
+
+    colNames = ["m0", "mw", "cid", "mf", "iupac", "synonyms", "url",
+            "is_in_kegg", "is_in_hmdb"]
+    colTypes = [float, float, int, str, str, str, str, int, int ]
+    colFormats=["%.6f", "%.6f", "%s", "%s", "%s", None, "%s", "%d", "%d" ]
+
+    @staticmethod
+    def _get_count():
+        url="http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
+        data = dict(db="pccompound",
+                    rettype="count",
+                    term="metabolic[SRCC] AND 0[TFC]",
+                    tool="emzed",
+                    email="tools@emzed.ethz.ch",
+                    )
+        r = requests.get(url, params=data)
+        if r.status_code != 200:
+            raise Exception("request %s failed.\nanswer : %s" % (r.url, r.text))
+        doc = dom_tree_from_bytes(r.content)
+        counts = doc.findall("Count")
+        assert len(counts)==1
+        count = int(counts[0].text)
+        return count
+
+    @staticmethod
+    def _get_uilist(retmax=None, source=None):
+        term="metabolic[SRCC] AND 0[TFC]"
+        if source is not None:
+            term +=' AND "%s"[SRC]' % source
+        if retmax is None:
+            retmax = 99999999
+        data = dict(db="pccompound",
+                    rettype="uilist",
+                    term=term,
+                    retmax=retmax,
+                    tool="emzed",
+                    email="tools@emzed.ethz.ch",
+                    usehistory="Y"
+                    )
+        url="http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
+        r = requests.get(url, params=data)
+        if r.status_code != 200:
+            raise Exception("request %s failed.\nanswer : %s" % (r.url, r.text))
+        doc = dom_tree_from_bytes(r.content)
+        if not doc.findall("IdList"):
+            raise Exception("Pubchem returned data in unknown format")
+        idlist = [int(id_.text) for id_ in  doc.findall("IdList")[0].findall("Id")]
+        return idlist
+
+    @staticmethod
+    def _get_summary_data(ids):
+        data = dict(db="pccompound",
+                    tool="emzed",
+                    email="tools@emzed.ethz.ch",
+                    id=",".join(str(id_) for id_ in ids),
+                    version="2.0"
+                    )
+        url="http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi"
+        r = requests.get(url, params=data)
+        if r.status_code != 200:
+            raise Exception("request %s failed.\nanswer : %s" % (r.url, r.text))
+        return r.text
+
+    @staticmethod
+    def _parse_data(data, keggIds=None, humanMBdbIds=None):
+        doc = dom_tree_from_bytes(data)
+        items = []
+        for summary in doc[0].findall("DocumentSummary"):
+            if len(summary.findall("error")):
+                print "RETRIEVAL FOR ID=%s FAILED" % (summary.attrib.get("uid"))
+                continue
+
+            dd = dict()
+            for name, type_, colName in [ ("CID", int, "cid"),
+                                          ("MolecularWeight", float, "mw"),
+                                          ("MolecularFormula", str, "mf"),
+                                          ("IUPACName", str, "iupac")]:
+                element = summary.find(name)
+                text = element.text
+                value = type_(text)
+                dd[colName] = value
+
+            synonyms = ";".join(t.text for t in summary.find("SynonymList"))
+            dd["synonyms"] = synonyms
+            dd["is_in_kegg"]=dd["cid"] in (keggIds or [])
+            dd["is_in_hmdb"]=dd["cid"] in (humanMBdbIds or [])
+            items.append(dd)
+        return items
+
+
+    @staticmethod
+    def _download(idlist, keggIds=None, humanMBdbIds=None):
+        print
+        print "START DOWNLOAD OF", len(idlist), "ITEMS"
+        sys.stdout.flush()
+        started = time.time()
+        batchsize = 500
+        jobs = [idlist[i:i+batchsize] for i in range(0, len(idlist), batchsize)]
+        items = []
+        for i, j in enumerate(jobs):
+            data = PubChemDB._get_summary_data(j)
+            if data is None:
+                print "FAILED TO CONNECT"
+                data = []
+
+            items.extend(PubChemDB._parse_data(data, keggIds, humanMBdbIds))
+            print "   %3d %%" % (100.0 * (i+1)/len(jobs)), "done",
+            needed = time.time()-started
+            time_per_batch = needed / (i+1)
+            remaining = time_per_batch * (len(jobs)-i-1)
+            print "   end of download in %.fm %.fs" % divmod(remaining, 60)
+            sys.stdout.flush()
+
+        needed = time.time()-started
+        print
+        print "TOTAL TIME %.fm %.fs" % divmod(needed,60)
+        return items
+
+    def __init__(self, path=None):
+        self.path = path
+        if path is not None and os.path.exists(path):
+            self.table = Table.load(path)
+            self.table.resetInternals()
+        else:
+            self.table = self._emptyTable()
+
+    def _emptyTable(self):
+        return Table(PubChemDB.colNames, PubChemDB.colTypes,
+                     PubChemDB.colFormats, [], "PubChem")
+
+    def __len__(self):
+        return len(self.table)
+
+    def getDiff(self, maxIds = None):
+        try:
+            counts = PubChemDB._get_count()
+            unknown = []
+            missing = []
+            if counts!=len(self.table):
+                uis = set(PubChemDB._get_uilist(maxIds))
+                if uis is not None:
+                    known_uis = set(self.table.cid.values)
+                    unknown = list(uis - known_uis)
+                    missing = list(known_uis-uis)
+            return unknown, missing
+        except Exception, e:
+            import traceback; traceback.print_exc()
+            return [], [] # failed
+
+    def reset(self):
+        self.table = self._emptyTable()
+        self.update()
+        self.store()
+
+    def massCalculator(self, table, row, name):
+        return monoisotopicMass(table.getValue(row, "mf"))
+
+    def update(self, maxIds=None):
+            self._update(maxIds)
+
+    def _update(self, maxIds):
+
+        newIds, missingIds = self.getDiff()
+        if maxIds is not None:
+            newIds = newIds[:maxIds] # for testing
+
+        keggids = set(PubChemDB._get_uilist(source="KEGG"))
+        hmdbids = set(PubChemDB._get_uilist(source="Human Metabolome Database"))
+
+        print "FETCH", len(newIds), "ITEMS"
+        if newIds:
+            for dd in PubChemDB._download(newIds, keggids, hmdbids):
+                row = [ dd.get(n) for n in self._colNames ]
+                self.table.rows.append(row)
+        try:
+            self.table.dropColumns("url")
+        except:
+            pass
+        try:
+            self.table.dropColumns("m0")
+        except:
+            pass
+        if len(missingIds):
+            print "DELETE", len(missingIds), "ENTRIES FROM LOCAL DB"
+            self.table = self.table.filter(~self.table.cid.isIn(missingIds))
+        url = "http://pubchem.ncbi.nlm.nih.gov/summary/summary.cgi?cid="
+        self.table.addColumn("url", url+self.table.cid.apply(str), type_=str)
+        self.table.addColumn("m0", self.massCalculator, type_=float, format_="%.7f", insertBefore="mw")
+        self.table.sortBy("m0")# build index
+
+    def store(self, path=None):
+        if path is None:
+            path = self.path
+        assert path is not None, "no path given in constructor nor as argument"
+        self.table.store(path, forceOverwrite=True)
+
+    def __getattr__(self, colName):
+        return getattr(self.table, colName)
+
+

emzed/core/explorers/__init__.py
@@ -1,0 +1,2 @@
+from peakmap_explorer import inspectPeakMap, PeakMapExplorer
+from table_explorer import inspect, TableExplorer

emzed/core/explorers/config.py
@@ -1,0 +1,83 @@
+from guiqwt.config import CONF
+
+def setupCommonStyle(line, marker):
+
+   markerSymbol = "Ellipse" # in this case a circle, because we give only one size parameter.
+   edgeColor = "#555555"
+   faceColor = "#cc0000"
+   alpha = 0.8
+   size = 6
+   params = {
+       "marker/cross/symbol/marker": markerSymbol,
+       "marker/cross/symbol/edgecolor": edgeColor,
+       "marker/cross/symbol/facecolor": faceColor,
+       "marker/cross/symbol/alpha": alpha,
+       "marker/cross/symbol/size": size,
+       "marker/cross/line/color" : "#000000",
+       #"marker/cross/line/width": 0.0,
+       "marker/cross/line/style": "NoPen",
+       }
+   CONF.update_defaults(dict(plot=params))
+   marker.markerparam.read_config(CONF, "plot", "marker/cross")
+   marker.markerparam.update_marker(marker)
+   params = {
+       "shape/drag/symbol/marker": markerSymbol,
+       "shape/drag/symbol/size": size,
+       "shape/drag/symbol/edgecolor": edgeColor,
+       "shape/drag/symbol/facecolor": faceColor,
+       "shape/drag/symbol/alpha": alpha,
+
+       }
+   CONF.update_defaults(dict(plot=params))
+   line.shapeparam.read_config(CONF, "plot", "shape/drag")
+   line.shapeparam.update_shape(line)
+
+def setupStyleRtMarker(marker):
+   linecolor = "#909090"
+   edgeColor = "#005500"
+   faceColor = "#005500"
+   params = {
+       "marker/cross/symbol/marker": "Rect",
+       "marker/cross/symbol/size": 0,
+       "marker/cross/symbol/edgecolor": edgeColor,
+       "marker/cross/symbol/facecolor": faceColor,
+       "marker/cross/line/color" : linecolor,
+       "marker/cross/line/width": 1.0,
+       "marker/cross/line/style": "SolidLine",
+
+       "marker/cross/sel_symbol/size": 0,
+       "marker/cross/sel_line/color" : linecolor,
+       "marker/cross/sel_line/width": 1.0,
+       "marker/cross/sel_line/style": "SolidLine",
+       }
+   CONF.update_defaults(dict(plot=params))
+   marker.markerparam.read_config(CONF, "plot", "marker/cross")
+   marker.markerparam.update_marker(marker)
+
+
+def setupStyleRangeMarker(range_):
+    params = {
+             "range/line/style" : 'SolidLine',
+             "range/line/color" : "gray", # "#ff9393",
+             "range/line/width" : 1,
+             "range/sel_line/style" : 'SolidLine',
+             "range/sel_line/color" : "gray", # "red",
+             "range/sel_line/width" : 1 + 0* 2,
+             "range/fill" : "gray",
+             "range/shade" : 0.10,
+             "range/symbol/marker" : "Ellipse",
+             "range/symbol/size" : 7+1,
+             "range/symbol/edgecolor" : "white",
+             "range/symbol/facecolor" : "gray", # "#ff9393",
+             "range/symbol/alpha" : 1 - 0.2,
+             "range/sel_symbol/marker" : "Ellipse",
+             "range/sel_symbol/size" : 9-1,
+             "range/sel_symbol/edgecolor" : "white",
+             "range/sel_symbol/facecolor" : "gray", # "red",
+             "range/sel_symbol/alpha" : .9,
+             "range/multi/color" : "#806060",
+            }
+
+    CONF.update_defaults(dict(histogram=params))
+    range_.shapeparam.read_config(CONF, "histogram", "range")
+    range_.shapeparam.update_range(range_)

emzed/core/explorers/helpers.py
@@ -1,0 +1,25 @@
+import functools
+
+def widthOfTableWidget(tw):
+
+    width = 0
+    for i in range(tw.columnCount()):
+        width += tw.columnWidth(i)
+
+    width += tw.verticalHeader().sizeHint().width()
+    width += tw.verticalScrollBar().sizeHint().width()
+    width += tw.frameWidth()*2
+    return width
+
+
+def protect_signal_handler(fun):
+    @functools.wraps(fun)
+    def wrapped(*a, **kw):
+        try:
+            return fun(*a, **kw)
+        except:
+            import traceback
+            traceback.print_exc()
+    return wrapped
+
+

commit 659bc759ac0a667d713444f23cc139afd1bb9970

[FIX] fixed issue caused by commit 910eeb69
    this small change had some impact: FilesOpenItems did not work any
    more and the errormessage when ok-ing a dialog with illegal values
    was not shown any more.

emzed/gui/dialog_builder.py
@@ -24,7 +24,7 @@ di.MultipleChoiceItem.check_value = _true
 di.ButtonItem.check_value = _true
 
 # patch ok / cancel: never check if needed fields are present ?!
-DataSetEditDialog.check = _true
+# DataSetEditDialog.check = _true
 
 
 def _patched_get(self, instance, klass):