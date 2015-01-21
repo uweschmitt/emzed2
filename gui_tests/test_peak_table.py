002dfe9a2a7bdc095d3a1b6fd49d56629d1a4f3e
uweschmitt <uwe.schmitt@id.ethz.ch>
2015-01-21 11:01:17 +0100

    SSDM-1361: improved test script for table explorer

--- gui_tests/test_peak_table.py
+++ gui_tests/test_peak_table.py
@@ -14,9 +14,9 @@ t = emzed.utils.toTable("mzmin", range(0, 2000, 100), type_=float)
 t.addColumn("mzmax", t.mzmin + 10.0)
 t.addColumn("rtmin", range(10, 30), type_=float)
 t.addColumn("rtmax", t.rtmin + 3)
-t.addColumn("peakmap", pm)
+#t.addColumn("peakmap", pm)
 t.addColumn("class", t.rtmin > 20)
-t = emzed.utils.integrate(t)
+# t = emzed.utils.integrate(t)
 
 print t.getColFormats()
 
@@ -25,9 +25,9 @@ t2 = emzed.utils.toTable("mzmin", range(0, 2000, 200), type_=float)
 t2.addColumn("mzmax", t2.mzmin + 10.0)
 t2.addColumn("rtmin", range(10, 30, 2), type_=float)
 t2.addColumn("rtmax", t2.rtmin + 3)
-t2.addColumn("peakmap", pm)
+#t2.addColumn("peakmap", pm)
 
-t2 = emzed.utils.integrate(t2)
+# t2 = emzed.utils.integrate(t2)
 
 emzed.gui.inspect((t, t2))
 
