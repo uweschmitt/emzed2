be8a61e2248704785e912ac492a3da0f25d4c4e3
Uwe Schmitt <uwe.schmitt@id.ethz.ch>
2015-01-22 04:48:36 -0800

    [FIX] switch reg tests for feature finders depending on pyopenms version

--- tests/test_feature_detectors.py
+++ tests/test_feature_detectors.py
@@ -1,6 +1,9 @@
-import emzed.ff
-import emzed.io
+import pyopenms
+import emzed
 import pytest
+
+
+IS_PYOPENMS_2 = pyopenms.__version__.startswith("2.")
 
 
 @pytest.mark.slow
@@ -21,7 +24,7 @@ def test_centwave(path, regtest):
 
 
 @pytest.mark.slow
-def testmatched_filter(path, regtest):
+def test_matched_filter(path, regtest):
 
     pm = emzed.io.loadPeakMap(path("data", "test_mini.mzXML"))
 
@@ -41,11 +44,26 @@ def testmatched_filter(path, regtest):
     table.print_(out=regtest)
 
 
-@pytest.mark.slow
-def test_metabo(path, regtest):
+def _test_metabo(path, regtest):
     pm = emzed.io.loadPeakMap(path("data", "test_mini.mzXML"))
     table = emzed.ff.runMetaboFeatureFinder(pm,
                                             common_noise_threshold_int=3000.0,
                                             common_chrom_peak_snr=3000.0)
 
     table.print_(out=regtest)
+
+
+# as we have regression tests where the output changes from pyopenms to pyopenms2
+# we have to distinguish both cases. the name of the file recording the output
+# of the tests depend on the functions names, so we use different function names
+# in order to force recording results in different files:
+
+if IS_PYOPENMS_2:
+    @pytest.mark.slow
+    def test_metabo_ff_of_pyopenms_2(path, regtest):
+        _test_metabo(path, regtest)
+
+else:
+    @pytest.mark.slow
+    def test_metabo_ff_of_pyopenms_1(path, regtest):
+        _test_metabo(path, regtest)
