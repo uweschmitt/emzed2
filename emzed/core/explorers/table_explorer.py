uweschmitt/emzed2

commit 6e7979f149a2f3618468f1c707efdb33a8fadcf0

SSDM-1361: tables now with columen type CallBack incl support in table explorer

emzed/core/explorers/table_explorer.py
@@ -9,7 +9,7 @@ import guidata
 
 from plotting_widgets import RtPlotter, MzPlotter
 
-from ..data_types import Table, PeakMap
+from ..data_types import Table, PeakMap, CallBack
 
 from table_explorer_model import *
 
@@ -47,6 +47,38 @@ def configsForSmootheds(smootheds):
 
 def configsForSpectra(n):
     return [dict(color=getColors(i), linewidth=1) for i in range(n)]
+
+
+class ButtonDelegate(QItemDelegate):
+
+    """
+    A delegate that places a fully functioning QPushButton in every
+    cell of the column to which it's applied
+
+    we have to distinguis view and parent here: using the view as parent does not work
+    in connection with modal dialogs opened in the click handler !
+    """
+
+    def __init__(self, view, parent):
+        QItemDelegate.__init__(self, parent)
+        self.view = view
+
+    def paint(self, painter, option, index):
+        if not self.view.indexWidget(index):
+            # we find the mode using the view, as the current model might change if one explores
+            # more than one table wit the table explorer:
+            model = self.view.model()
+            cell = model.cell_value(index)
+            label = model.data(index)
+            row = model.row(index)
+
+            parent = self.parent()   # this is the table explorer
+
+            def clicked(__):
+                cell.callback(row, parent)
+
+            button = QPushButton(label, self.parent(), clicked=clicked)
+            self.view.setIndexWidget(index, button)
 
 
 class EmzedTableView(QTableView):
@@ -202,6 +234,12 @@ class TableExplorer(EmzedDialog):
     def setupTableViewFor(self, model):
 
         tableView = EmzedTableView(self)
+
+        table = model.table
+        for i, t in enumerate(table.getColTypes()):
+            if t == CallBack:
+                bd = ButtonDelegate(tableView, self)
+                tableView.setItemDelegateForColumn(i, bd)
 
         tableView.setModel(model)
         tableView.horizontalHeader().setResizeMode(QHeaderView.Interactive)

emzed/core/explorers/table_explorer_model.py
@@ -325,6 +325,10 @@ class TableModel(QAbstractTableModel):
         value = self.table.rows[ridx][cidx]
         return value
 
+    def row(self, index):
+        ridx, cidx = self.table_index(index)
+        return self.table.getValues(self.table.rows[ridx])
+
     def data(self, index, role=Qt.DisplayRole):
         if not index.isValid():
             return QVariant()

emzed/core/__init__.py
@@ -1,1 +1,1 @@
-from data_types import Table, PeakMap, Spectrum
+from data_types import Table, PeakMap, Spectrum, CallBack

emzed/core/data_types/__init__.py
@@ -1,2 +1,2 @@
 from ms_types import PeakMap, Spectrum
-from table    import Table
+from table    import Table, CallBack

emzed/core/data_types/table.py
@@ -31,14 +31,27 @@ __doc__ = """
 def deprecation(message):
     warnings.warn(message, UserWarning, stacklevel=3)
 
-standardFormats = {int: "%d", long: "%d", float: "%.2f", str: "%s"}
 
-import emzed
 
+class CallBack(object):
+
+    def __init__(self, label, callback):
+        self.label = label
+        self.callback = callback
+
+    def __str__(self):
+        return self.label
+
+
+standardFormats = {int: "%d", long: "%d", float: "%.2f", str: "%s", CallBack: "%s"}
+
+fms = "'%.2fm' % (o/60.0)"  # format seconds to floating point minutes
 
 formatSeconds = fms
 
 formatHexId = "'%x' % id(o)"
+
+
 
 
 def guessFormatFor(name, type_):

emzed/utils/__init__.py
@@ -5,7 +5,7 @@ from formula_generator  import formulaTable
 
 from integration import integrate
 
-from ..core.data_types.table import Table
+from ..core.data_types.table import Table, CallBack
 
 mergeTables = Table.mergeTables
 stackTables = Table.stackTables

gui_tests/test_table_with_callback_column.py
@@ -1,0 +1,21 @@
+# encoding: utf-8
+
+
+import emzed
+CallBack = emzed.core.CallBack
+
+
+def show(row, parent):
+    expl = emzed.core.explorers.TableExplorer([row.table], False, parent=parent)
+    expl.raise_()
+    expl.show()
+
+t = emzed.utils.toTable("a", (1, 2, 3))
+t2 = emzed.utils.toTable("t", (2, 3, 4, 6))
+
+t.addColumn("table", t2)
+
+t.addColumn("job", t.a.apply(lambda v: CallBack("press me %d" % v, show)))
+
+print(t)
+emzed.gui.inspect(t)

commit d0efdd5a03f2b5aabc5e4f5c47dc1a9a9cca21b3

SSDM-1361: fixed regression from 6e7979f1 when hiding columns in table explorer

emzed/core/explorers/table_explorer.py
@@ -231,15 +231,22 @@ class TableExplorer(EmzedDialog):
         else:
             return None
 
+    def set_delegates(self):
+        bd = ButtonDelegate(self.tableView, self)
+        types = self.model.table.getColTypes()
+        for i, j in self.model.widgetColToDataCol.items():
+            if types[j] == CallBack:
+                self.tableView.setItemDelegateForColumn(i, bd)
+
+    def remove_delegates(self):
+        types = self.model.table.getColTypes()
+        for i, j in self.model.widgetColToDataCol.items():
+            if types[j] == CallBack:
+                self.tableView.setItemDelegateForColumn(i, None)
+
     def setupTableViewFor(self, model):
 
         tableView = EmzedTableView(self)
-
-        table = model.table
-        for i, t in enumerate(table.getColTypes()):
-            if t == CallBack:
-                bd = ButtonDelegate(tableView, self)
-                tableView.setItemDelegateForColumn(i, bd)
 
         tableView.setModel(model)
         tableView.horizontalHeader().setResizeMode(QHeaderView.Interactive)
@@ -517,6 +524,7 @@ class TableExplorer(EmzedDialog):
 
     @protect_signal_handler
     def choose_visible_columns(self, *a):
+        self.remove_delegates()
         col_names, is_currently_visible = self.model.columnames_with_visibility()
         dlg = ColumnMultiSelectDialog(col_names, is_currently_visible)
         dlg.exec_()
@@ -524,6 +532,7 @@ class TableExplorer(EmzedDialog):
             return
         visible_cols = [col_idx for (n, col_idx, visible) in dlg.result if visible]
         self.model.set_visilbe_cols(visible_cols)
+        self.set_delegates()
 
     @protect_signal_handler
     def remove_filtered(self, *a):
@@ -616,6 +625,7 @@ class TableExplorer(EmzedDialog):
         self.model = self.models[i]
         self.current_filter_widget = self.filterWidgets[i]
         self.tableView = self.tableViews[i]
+        self.set_delegates()
         self.setupModelDependendLook()
         if self.isIntegrated:
             self.model.setNonEditable("method", ["area", "rmse", "method", "params"])

emzed/core/explorers/table_explorer_model.py
@@ -640,7 +640,6 @@ class TableModel(QAbstractTableModel):
         self.endResetModel()
         self.emit_data_change()
 
-
     def emit_data_change(self):
         visible_table = self.table[self.widgetRowToDataRow.values()]
         self.DATA_CHANGE.emit(self.table, visible_table)