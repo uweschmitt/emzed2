8877de21a71d356b2f66a500cb9ee9adc3037dbc
uweschmitt <uwe.schmitt@id.ethz.ch>
2015-01-27 10:50:29 +0100

    SSDM-1361: litte improvement for gui_tests/test_table_with_callback_column.py

--- gui_tests/test_table_with_callback_column.py
+++ gui_tests/test_table_with_callback_column.py
@@ -1,36 +1,26 @@
 # encoding: utf-8
 
-
 import emzed
 CallBack = emzed.core.CallBack
 
 
+# first colum containts tables:
 subtable = emzed.utils.toTable("t", (2, 3, 4, 6))
-
-t = emzed.utils.toTable("number", (1, 2, 3))
+t = emzed.utils.toTable("number", (42, 4711, 666))
 t.addColumn("table", subtable)
 
 
 def show(row, parent):
+    print "you clicked on row with values", row
     emzed.gui.inspect(row.table, modal=False, parent=parent)
 
-# all cells in column are the same:
-t.addColumn("button_1", CallBack("press me", show))
+def message(row, parent):
+    emzed.gui.showInformation("you clicked on row with number %d" % row.number)
 
 # create three cells of type CallBack manually:
+cb1 = CallBack("show subtable", show)
+cb2 = CallBack("yo !?", message)
 
-cb1 = CallBack("hi", show)
-cb2 = CallBack("yo", show)
-cb3 = CallBack("whats up", show)
-
-t.addColumn("button_2", [cb1, cb2, cb3])
-
-# create three cells of type CallBack by computation using t.number.apply:
-
-
-def create_call_back(i):
-    return CallBack("press me %d" % i, show)
-
-t.addColumn("button_3", t.number.apply(create_call_back))
+t.addColumn("buttons", [cb1, cb2, cb2])
 
 emzed.gui.inspect(t)
