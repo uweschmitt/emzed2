uweschmitt/emzed2

commit 936c2d3c5767f4e590c99ffa7f03d2e237fd6f8d

fixed column type conversion when loading csv files with mixed types in a column

emzed/io/load_utils.py
@@ -57,42 +57,25 @@ def loadTable(path=None, compress_after_load=True):
 
 
 def loadCSV(path=None, sep=";", keepNone=False, **specialFormats):
-    """local import in order to keep namespaces clean"""
-    import csv
-    import os.path
-    import re
+    """
+    loads csv file from path. column separator is given by *sep*.
+    If *keepNone* is set to True, "None" strings in file are kept as a string.
+    Else this string is converted to Python None values.
+    *specialFormats* collects positional arguments for setting formats
+    of columns.
 
-    from ..core.data_types.table import (Table, common_type_for, bestConvert, guessFormatFor)
+    Example: ``emzed.io.loadCSV("abc.csv", mz="%.3f")``
 
-    path = _prepare_path(path, extensions=["csv"])
+    """
+
+    from ..core.data_types import Table
+
+    path = _prepare_path(path, extensions=["table"])
     if path is None:
         return None
 
-    with open(path, "rU") as fp:
-        # remove clutter at right margin
-        reader = csv.reader(fp, delimiter=sep)
-        # reduce multiple spaces to single underscore
-        colNames = [re.sub(" +", "_", n.strip()) for n in reader.next()]
-
-        if keepNone:
-            conv = bestConvert
-        else:
-            conv = lambda v: None if v == "None" else bestConvert(v)
-
-        rows = [[conv(c.strip()) for c in row] for row in reader]
-
-    columns = [[row[i] for row in rows] for i in range(len(colNames))]
-    types = [common_type_for(col) for col in columns]
-
-    # defaultFormats = {float: "%.2f", str: "%s", int: "%d"}
-    formats = dict([(name, guessFormatFor(name, type_)) for (name, type_) in zip(colNames, types)])
-    formats.update(specialFormats)
-
-    formats = [formats[n] for n in colNames]
-
-    title = os.path.basename(path)
-    meta = dict(loaded_from=os.path.abspath(path))
-    return Table._create(colNames, types, formats, rows, title, meta)
+    result = Table.loadCSV(path)
+    return result
 
 
 def loadBlob(path=None):

emzed/core/data_types/table.py
@@ -1624,6 +1624,11 @@ class Table(object):
         columns = [[row[i] for row in rows] for i in range(len(colNames))]
         types = [common_type_for(col) for col in columns]
 
+        # now convert columns to their common type:
+        for row in rows:
+            for i, (v, t) in enumerate(zip(row, types)):
+                row[i] = t(v)
+
         formats = dict([(name, guessFormatFor(name, type_)) for (name, type_)
                         in zip(colNames, types)])
         formats.update(specialFormats)

tests/test_csv_parsing.py
@@ -1,4 +1,5 @@
-import emzed.io
+import pdb
+import emzed
 import os.path
 
 def test_csv_parsing():
@@ -6,3 +7,21 @@ def test_csv_parsing():
     tab = emzed.io.loadCSV(os.path.join(here, "data", "mass.csv"))
     #fms = '"%.2fm" % (o/60.0)'
     assert tab.getFormat("RT_min") == "%.2f"
+
+
+def test_type_converion(tmpdir):
+    t = emzed.utils.toTable("id", (1, "1A"))
+    # test conversion
+    assert t.id.values == ("1", "1A")
+
+    # dirty addition
+    t.rows.append([2])
+    t.resetInternals()
+    assert t.id.values == ("1", "1A", 2)
+
+    path = tmpdir.join("1.csv").strpath
+    
+    emzed.io.storeCSV(t, path)
+    t = emzed.io.loadCSV(path)
+    assert t.id.values == ("1", "1A", "2")
+    assert t.getColTypes() == [str]

commit 58c08c392666c45fd5deae790f72f69b857216d4

SSDM-1407: fixed file extension for loadCSV, regression from 936c2d3c

emzed/io/load_utils.py
@@ -70,7 +70,7 @@ def loadCSV(path=None, sep=";", keepNone=False, **specialFormats):
 
     from ..core.data_types import Table
 
-    path = _prepare_path(path, extensions=["table"])
+    path = _prepare_path(path, extensions=["csv"])
     if path is None:
         return None
 