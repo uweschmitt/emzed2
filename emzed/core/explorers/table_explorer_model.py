uweschmitt/emzed2

commit 91a1f14e07861741242d2d7a53137fed7556f4da

SSDM-1407: keep selected rows if filtering of table is noop

emzed/core/explorers/table_explorer_model.py
@@ -37,12 +37,18 @@ class TableModel(QAbstractTableModel):
         self.widgetColToDataCol = dict(enumerate(self.indizesOfVisibleCols))
         nr = len(table)
         self.widgetRowToDataRow = dict(zip(range(nr), range(nr)))
+        self.dataRowToWidgetRow = dict(zip(range(nr), range(nr)))
         self.emptyActionStack()
 
         self.nonEditables = set()
 
         self.last_limits = None
         self.setFiltersEnabled(False)
+
+        self.selected_data_rows = []
+
+    def set_selected_data_rows(self, widget_rows):
+        self.selected_data_rows = self.transform_row_idx_widget_to_model(widget_rows)
 
     def setFiltersEnabled(self, flag):
         self.filters_enabled = flag
@@ -438,14 +444,22 @@ class TableModel(QAbstractTableModel):
                     rows_to_remain.add(j)
             all_rows_to_remain = all_rows_to_remain.intersection(rows_to_remain)
 
-        self.beginResetModel()
-        self.widgetRowToDataRow = dict()
-        self.dataRowToWidgetRow = dict()
+        widget_row_to_data_row = {}
+        data_row_to_widget_row = {}
         for view_idx, row_idx in enumerate(sorted(all_rows_to_remain)):
-            self.widgetRowToDataRow[view_idx] = row_idx
-            self.dataRowToWidgetRow[row_idx] = view_idx
-        self.endResetModel()
-        self.emit_data_change()
+            widget_row_to_data_row[view_idx] = row_idx
+            data_row_to_widget_row[row_idx] = view_idx
+
+        if widget_row_to_data_row != self.widgetRowToDataRow:
+            # only reset view if something changed
+
+            self.beginResetModel()
+            self.widgetRowToDataRow = dict()
+            self.dataRowToWidgetRow = dict()
+            self.widgetRowToDataRow = widget_row_to_data_row
+            self.dataRowToWidgetRow = data_row_to_widget_row
+            self.endResetModel()
+            self.emit_data_change()
 
     def emit_data_change(self):
         visible_table = self.table[self.widgetRowToDataRow.values()]

emzed/core/data_types/ms_types.py
@@ -1,4 +1,3 @@
-import pdb
 import pyopenms
 import numpy as np
 import os.path
@@ -417,7 +416,7 @@ class PeakMap(object):
 
         if OPTIMIZATIONS_INSTALLED:
             rts, iis = emzed_optimizations.chromatogram(self, mzmin, mzmax, rtmin, rtmax, msLevel)
-            # fix bug in implementation of emzed_optimizations:
+            # fix bug in old version of emzed_optimizations:
             # fails if rtmin and rtmax are beyond max rt in peakmap !
             f = (rts >= rtmin) * (rts <= rtmax)
             return rts[f], iis[f]

emzed/core/explorers/table_explorer.py
@@ -107,8 +107,8 @@ class EmzedTableView(QTableView):
     @protect_signal_handler
     def keyPressEvent(self, evt):
         if (evt.modifiers(), evt.key()) == (Qt.ControlModifier, Qt.Key_F):
-            if self.selectedIndexes():
-                column = self.selectedIndexes()[0].column()
+            if self.model.selectedIndexes():
+                column = self.model.selectedIndexes()[0].column()
             else:
                 column = self.model().current_sort_col_idx
 
@@ -144,7 +144,6 @@ class TableExplorer(EmzedDialog):
         self.model = None
         self.tableView = None
 
-        self.selected_data_rows = []
         self.hadFeatures = None
         self.wasIntegrated = None
 
@@ -701,7 +700,6 @@ class TableExplorer(EmzedDialog):
     def handle_model_reset(self):
         for name in self.model.table.getColNames():
             self.current_filter_widget.update(name)
-        self.selected_data_rows = []
 
     @protect_signal_handler
     def dataChanged(self, ix1, ix2, src):
@@ -722,7 +720,7 @@ class TableExplorer(EmzedDialog):
 
         if self.hasFeatures:
             # minr, maxr = sorted((ix1.row(), ix2.row()))
-            if any(minr <= index <= maxr for index in self.selected_data_rows):
+            if any(minr <= index <= maxr for index in self.model.selected_data_rows):
                 if isinstance(src, IntegrateAction):
                     self.updatePlots(reset=False)
                 else:
@@ -771,7 +769,7 @@ class TableExplorer(EmzedDialog):
         # entry in the QComboBox which we have to remove now:
         postfix = str(self.choosePostfix.currentText()).strip("'")
         rtmin, rtmax = self.rt_plotter.getRangeSelectionLimits()
-        for data_row_idx in self.selected_data_rows:
+        for data_row_idx in self.model.selected_data_rows:
             self.model.integrate(postfix, data_row_idx, method, rtmin, rtmax)
 
     @protect_signal_handler
@@ -798,7 +796,7 @@ class TableExplorer(EmzedDialog):
             self.tableView.setSelectionMode(mode_before)
             self.tableView.verticalScrollBar().setValue(scrollbar_before)
 
-        self.selected_data_rows = self.model.transform_row_idx_widget_to_model(to_select)
+        self.model.set_selected_data_rows(to_select)
 
         if self.hasFeatures:
             self.rt_plotter.setEnabled(True)
@@ -815,7 +813,7 @@ class TableExplorer(EmzedDialog):
             self.chooseSpectrum.removeItem(0)
 
         postfixes, spectra = [], []
-        for idx in self.selected_data_rows:
+        for idx in self.model.selected_data_rows:
             pf, s = self.model.getLevelNSpectra(idx, minLevel=2)
             postfixes.extend(pf)
             spectra.extend(s)
@@ -848,7 +846,7 @@ class TableExplorer(EmzedDialog):
         mzmins, mzmaxs, rtmins, rtmaxs = [], [], [], []
 
         if self.hasEIConly:
-            for idx in self.selected_data_rows:
+            for idx in self.model.selected_data_rows:
                 eics, rtmin, rtmax, allrts = self.model.getEICs(idx)
                 if rtmin is not None:
                     rtmins.append(rtmin)
@@ -857,7 +855,7 @@ class TableExplorer(EmzedDialog):
                 curves.extend(eics)
 
         else:
-            for idx in self.selected_data_rows:
+            for idx in self.model.selected_data_rows:
                 eics, mzmin, mzmax, rtmin, rtmax, allrts = self.model.extractEICs(idx)
                 if mzmin is not None:
                     mzmins.append(mzmin)
@@ -928,7 +926,7 @@ class TableExplorer(EmzedDialog):
             callback """
         rtmin = self.rt_plotter.minRTRangeSelected
         rtmax = self.rt_plotter.maxRTRangeSelected
-        peakmaps = [pm for idx in self.selected_data_rows for pm in self.model.getPeakmaps(idx)]
+        peakmaps = [pm for idx in self.model.selected_data_rows for pm in self.model.getPeakmaps(idx)]
         mzmin = mzmax = None
         if resetLimits:
             mzmin, mzmax = resetLimits

gui_tests/test_peak_table.py
@@ -17,7 +17,7 @@ t.addColumn("rtmax", t.rtmin + 3)
 t.addColumn("peakmap", pm)
 t.addColumn("class", t.rtmin > 20)
 t = emzed.utils.integrate(t)
-t.dropColumns("peakmap", "rtmin")
+# t.dropColumns("peakmap", "rtmin")
 t.addEnumeration()
 t.addColumn("f", t.id / 3)
 

commit 4295caf4076a319fe35a64590a88a05dc0dcc74f

SSDM-1407: fixed broken table sort from commit 91a1f14

emzed/core/explorers/table_explorer_model.py
@@ -195,7 +195,7 @@ class TableModel(QAbstractTableModel):
             action = self.actions.pop()
             self.beginResetModel()
             action.undo()
-            self.update_visible_rows_for_given_limits()  # does endResetModel
+            self.update_visible_rows_for_given_limits(force_reset=True)  # does endResetModel
             self.redoActions.append(action)
             self.view.updateMenubar()
 
@@ -204,7 +204,7 @@ class TableModel(QAbstractTableModel):
             action = self.redoActions.pop()
             self.beginResetModel()
             action.do()
-            self.update_visible_rows_for_given_limits()  # does endResetModel
+            self.update_visible_rows_for_given_limits(force_reset=True)  # does endResetModel
             self.actions.append(action)
             self.view.updateMenubar()
             return
@@ -232,13 +232,15 @@ class TableModel(QAbstractTableModel):
             self.beginResetModel()
             self.runAction(SortTableAction, dataColIdx, colIdx, order)
             self.current_sort_col_idx = colIdx
-            self.update_visible_rows_for_given_limits()  # does endResetModel
+            self.update_visible_rows_for_given_limits(force_reset=True)  # does endResetModel
+            #self.endResetModel()
+            #self.emit_data_change()
 
     def integrate(self, data_row_idx, postfix, method, rtmin, rtmax):
         self.beginResetModel()
         self.runAction(IntegrateAction, postfix, data_row_idx, method, rtmin, rtmax,
                        self.dataRowToWidgetRow)
-        self.update_visible_rows_for_given_limits()  # does endResetModel
+        self.update_visible_rows_for_given_limits(force_reset=False)  # does endResetModel
 
     def eicColNames(self):
         return ["peakmap", "mzmin", "mzmax", "rtmin", "rtmax"]
@@ -419,7 +421,7 @@ class TableModel(QAbstractTableModel):
         self.last_limits = limits
         self.update_visible_rows_for_given_limits()
 
-    def update_visible_rows_for_given_limits(self):
+    def update_visible_rows_for_given_limits(self, force_reset=False):
         if self.filters_enabled is False:
             limits = {}
         else:
@@ -450,7 +452,7 @@ class TableModel(QAbstractTableModel):
             widget_row_to_data_row[view_idx] = row_idx
             data_row_to_widget_row[row_idx] = view_idx
 
-        if widget_row_to_data_row != self.widgetRowToDataRow:
+        if force_reset or widget_row_to_data_row != self.widgetRowToDataRow:
             # only reset view if something changed
 
             self.beginResetModel()