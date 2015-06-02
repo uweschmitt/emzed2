39f74f2d1b0eeec259e333ae1bd8a8e24c02ad71
uweschmitt <uwe.schmitt@id.ethz.ch>
2015-06-02 14:51:07 +0200

    SSDM-1847 : reelase 2.12.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 # no import emzed here, causes trouble when installing on win, as missing packages
 # are needed when importing emzed
-version = (2, 12, 4)
+version = (2, 12, 5)
 
 setup(name="emzed",
       # keywords=["stable"],
