uweschmitt/emzed2

commit 703e6d56097a965d83bc6e4943a5e017ce122c7c

SSDM-1874: fix broken startup if active project folder is not found

emzed/project/project.py
@@ -352,7 +352,7 @@ def start_work(name=None, project_home=None):
 
 
 def activate_last_project():
-    """ checks emezd config for current project and activates this """
+    """ checks emzed config for current project and activates this """
 
     last_project = global_config.get("last_active_project")
     project_home = global_config.get("project_home")
@@ -364,5 +364,9 @@ def activate_last_project():
         path_in_project_home = os.path.join(project_home, last_project)
         _set_active_project(path_in_project_home)
         print "CWD TO", path_in_project_home
-        os.chdir(path_in_project_home)
-        _install_builtins_after_workon()
+        try:
+            os.chdir(path_in_project_home)
+        except:
+            print "could not find project", last_active_project
+        else:
+            _install_builtins_after_workon()

commit 2367020a3b49b2fcb21b0c8aa1d315bb53b25810

SSDM-1847 : fixed regression from 703e6d56

emzed/project/project.py
@@ -354,14 +354,14 @@ def start_work(name=None, project_home=None):
 def activate_last_project():
     """ checks emzed config for current project and activates this """
 
-    last_project = global_config.get("last_active_project")
+    last_active_project = global_config.get("last_active_project")
     project_home = global_config.get("project_home")
 
-    if last_project:
-        # start_work(last_project) # does not work, crashes on win maybe bcause
+    if last_active_project:
+        # start_work(last_active_project) # does not work, crashes on win maybe bcause
         # starting pythonin subprocess
         import os
-        path_in_project_home = os.path.join(project_home, last_project)
+        path_in_project_home = os.path.join(project_home, last_active_project)
         _set_active_project(path_in_project_home)
         print "CWD TO", path_in_project_home
         try: