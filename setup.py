bd1a03d2d7f84912b1b7f6fde42a3436bbd8b1e2
uweschmitt <uwe.schmitt@id.ethz.ch>
2015-06-15 15:14:19 +0200

    SSDM-1889 : release v 2.12.11

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 # no import emzed here, causes trouble when installing on win, as missing packages
 # are needed when importing emzed
-version = (2, 12, 10)
+version = (2, 12, 11)
 
 
 setup(name="emzed",
