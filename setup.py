de434a70a4004958af046f7d276e502db0c19dd7
uweschmitt <uwe.schmitt@id.ethz.ch>
2015-08-06 16:21:28 +0200

    SSDM-2268 : release 2.18.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 # no import emzed here, causes trouble when installing on win, as missing packages
 # are needed when importing emzed
-version = (2, 18, 3)
+version = (2, 18, 4)
 
 
 setup(name="emzed",
