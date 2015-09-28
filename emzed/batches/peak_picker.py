uweschmitt/emzed2

commit 1aa69ea92dac41a184c870367da76bc693ebcb41

SSDM-2509: _algorithm_configs.py -> algorithm_configs.py + partial pep8 cleanup

emzed/batches/peak_picker.py
@@ -1,5 +1,4 @@
 def runPeakPickerHiRes(pattern=None, destination=None, configid=None, **params):
-
     """ import runPeakPickerHiRes.
     runs peakPickerHiRes from openMs in batch mode.
     - input files are map files (mzXML, mxML, mzData),
@@ -18,7 +17,7 @@ def runPeakPickerHiRes(pattern=None, destination=None, configid=None, **params):
     If you have a single config this one is used automatically.
 
     Examples:
-    
+
     - runPeakPickerHiRes()
        asks for source files and target directory
        asks for config if multiple algorithm_configs are defined
@@ -50,9 +49,8 @@ def runPeakPickerHiRes(pattern=None, destination=None, configid=None, **params):
        runs peak picking with modified parameter
     """
 
-
     from ..core.batch_runner import BatchRunner
-    from .. import _algorithm_configs
+    from .. import algorithm_configs
     from .. import io
     import os.path
     from ..core import peak_picking
@@ -72,13 +70,13 @@ def runPeakPickerHiRes(pattern=None, destination=None, configid=None, **params):
                 print "reading FAILED"
                 return None
 
-            picked = self.pp.pickPeakMap(pm, showProgress = True)
+            picked = self.pp.pickPeakMap(pm, showProgress=True)
             return picked
 
         def write(self, result, destinationDir, path):
             basename, ext = os.path.splitext(os.path.basename(path))
-            savePath = os.path.join(destinationDir, basename+"_centroided.mzML")
+            savePath = os.path.join(destinationDir, basename * "_centroided.mzML")
             print "save to ", savePath
             io.storePeakMap(result, savePath)
 
-    return P(_algorithm_configs.peakPickerHiResConfig, False).run(pattern, destination, configid, **params)
+    return P(algorithm_configs.peakPickerHiResConfig, False).run(pattern, destination, configid, **params)

emzed/batches/__init__.py
@@ -1,6 +1,7 @@
 from feature_detector_batches import runCentwave, runMatchedFilter, runMetaboFeatureFinder
 from peak_picker import runPeakPickerHiRes
+
 try:
     del feature_detector_batches
-except:
+except NameError:
     pass

emzed/batches/feature_detector_batches.py
@@ -1,6 +1,7 @@
 
 from ..core import batch_runner
 
+
 class FD(batch_runner.BatchRunner):
 
     def process(self, path):
@@ -65,7 +66,7 @@ def runCentwave(pattern=None, destination=None, configid="std", **params):
     :download:`Docs from XCMS library <../emzed/core/r_connect/centwave.txt>`
     """
 
-    from .. import _algorithm_configs
+    from .. import algorithm_configs
     from ..core.r_connect import CentwaveFeatureDetector
 
     class P(FD):
@@ -73,7 +74,7 @@ def runCentwave(pattern=None, destination=None, configid="std", **params):
         def setup(self, config):
             self.det = CentwaveFeatureDetector(**config)
 
-    return P(_algorithm_configs.centwaveConfig, True).run(pattern, destination, configid, **params)
+    return P(algorithm_configs.centwaveConfig, True).run(pattern, destination, configid, **params)
 
 
 def runMatchedFilter(pattern=None, destination=None, configid="std", **params):
@@ -120,20 +121,18 @@ def runMatchedFilter(pattern=None, destination=None, configid="std", **params):
     """
 
     from ..core.r_connect import MatchedFilterFeatureDetector
-    from .. import _algorithm_configs
+    from .. import algorithm_configs
 
     class P(FD):
 
         def setup(self, config):
             self.det = MatchedFilterFeatureDetector(**config)
 
-    return P(_algorithm_configs.matchedFilterConfig, True).run(pattern, destination, configid,
-                                                               **params)
-
+    return P(algorithm_configs.matchedFilterConfig, True).run(pattern, destination, configid,
+                                                              **params)
 
 
 def runMetaboFeatureFinder(pattern=None, destination=None, configid="std", **params):
-
     """runs *MetaboFeatureFinding* from *OpenMS* in batch mode.
 
     - *pattern* is a used for file globbing, eg "/data/experiment1/\*.mzML", allowed are
@@ -176,7 +175,7 @@ def runMetaboFeatureFinder(pattern=None, destination=None, configid="std", **par
     For the available parameter settings see :py:func:`~emzed.ff.runMetaboFeatureFinder`.
     """
 
-    from .. import _algorithm_configs
+    from .. import algorithm_configs
 
     class P(FD):
 
@@ -204,4 +203,4 @@ def runMetaboFeatureFinder(pattern=None, destination=None, configid="std", **par
         def setup(self, config):
             self._ff_config = config
 
-    return P(_algorithm_configs.metaboFFConfigs, True).run(pattern, destination, configid, **params)
+    return P(algorithm_configs.metaboFFConfigs, True).run(pattern, destination, configid, **params)

emzed/core/explorers/table_explorer.py
@@ -407,8 +407,8 @@ class TableExplorer(EmzedDialog):
     def setupIntegrationWidgets(self):
         self.intLabel = QLabel("Integration")
         self.chooseIntMethod = QComboBox()
-        from ... import _algorithm_configs
-        for name, _ in _algorithm_configs.peakIntegrators:
+        from ... import algorithm_configs
+        for name, _ in algorithm_configs.peakIntegrators:
             self.chooseIntMethod.addItem(name)
 
         self.choosePostfix = QComboBox()

emzed/core/explorers/table_explorer_model.py
@@ -13,7 +13,7 @@ import os
 import re
 from datetime import datetime
 
-from ... import _algorithm_configs
+from ... import algorithm_configs
 
 
 from .table_explorer_model_actions import *
@@ -346,7 +346,7 @@ class TableModel(QAbstractTableModel):
             values = self.getIntegrationValues(data_row_idx, p)
             method = values["method" + p]
             params = values["params" + p]
-            integrator = dict(_algorithm_configs.peakIntegrators).get(method)
+            integrator = dict(algorithm_configs.peakIntegrators).get(method)
             if method is not None:
                 # data is a tuple with two onedim numpy arrays:
                 data = integrator.getSmoothed(rts, params)

emzed/core/explorers/table_explorer_model_actions.py
@@ -4,7 +4,7 @@ from __future__ import print_function
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 
-from ... import _algorithm_configs
+from ... import algorithm_configs
 
 
 class TableAction(object):
@@ -175,7 +175,7 @@ class IntegrateAction(TableAction):
                            postfix=postfix)
 
     def do(self):
-        integrator = dict(_algorithm_configs.peakIntegrators).get(self.method)
+        integrator = dict(algorithm_configs.peakIntegrators).get(self.method)
         table = self.model.table
         # returns Bunch which sublcasses dict
         args = table.getValues(table.rows[self.data_row_idx])

emzed/ff/_metaboff.py
@@ -20,11 +20,11 @@ class _ParamHandler(object):
 
         common_params = pyopenms.Param()
         common_params.setValue("noise_threshold_int", 10.0,
-                "Intensity threshold below which peaks are regarded as noise.")
+                               "Intensity threshold below which peaks are regarded as noise.")
         common_params.setValue("chrom_peak_snr", 3.0,
-                            "Minimum signal-to-noise a mass trace should have")
+                               "Minimum signal-to-noise a mass trace should have")
         common_params.setValue("chrom_fwhm", 5.0,
-                           "Expected chromatographic peak width (in seconds).")
+                               "Expected chromatographic peak width (in seconds).")
 
         ffm_params = pyopenms.FeatureFindingMetabo().getDefaults()
         ffm_params.remove("chrom_fwhm")
@@ -58,7 +58,6 @@ class _ParamHandler(object):
         ffm_params.remove("chrom_peak_snr")
 
         return mtd_params, epdet_params, ffm_params, params
-
 
     @staticmethod
     def setup_doc_string():
@@ -113,7 +112,7 @@ class _ParamHandler(object):
 
 
 def metaboFeatureFinder(peak_map, config_id=None, ms_level=None, **kw):
-    from .._algorithm_configs import metaboFFConfigs
+    from ..algorithm_configs import metaboFFConfigs
 
     config_params = dict()
 
@@ -137,7 +136,7 @@ def metaboFeatureFinder(peak_map, config_id=None, ms_level=None, **kw):
     start_at = time.time()
 
     (mtd_params, epdet_params,
-           ffm_params, all_params) = _ParamHandler.update_params(config_params)
+     ffm_params, all_params) = _ParamHandler.update_params(config_params)
 
     def dump_param(prefix, all_params=all_params):
         sub_params = all_params.copy(prefix)
@@ -200,8 +199,7 @@ def metaboFeatureFinder(peak_map, config_id=None, ms_level=None, **kw):
             final_mass_traces = splitted_mass_traces
 
         info("%d SPLITTED MASS TRACES AFTER ELUTION PEAK DETECTION",
-                len(final_mass_traces))
-
+             len(final_mass_traces))
 
         ffm = pyopenms.FeatureFindingMetabo()
         ffm.setParameters(ffm_params)
@@ -213,11 +211,11 @@ def metaboFeatureFinder(peak_map, config_id=None, ms_level=None, **kw):
         for i, feature in enumerate(feature_map):
             convex_hulls = feature.getConvexHulls()
             quality = feature.getOverallQuality()
-            width   = feature.getWidth()
-            z  = feature.getCharge()
+            width = feature.getWidth()
+            z = feature.getCharge()
             mz = feature.getMZ()
             rt = feature.getRT()
-            I  = feature.getIntensity()
+            I = feature.getIntensity()
             for convex_hull in convex_hulls:
                 bb = convex_hull.getBoundingBox()
                 rtmin, mzmin = bb.minPosition()
@@ -227,11 +225,11 @@ def metaboFeatureFinder(peak_map, config_id=None, ms_level=None, **kw):
                 rows.append(row)
 
     tab = Table(["feature_id", "mz", "mzmin", "mzmax", "rt", "rtmin", "rtmax",
-                    "intensity", "quality", "fwhm", "z"],
+                 "intensity", "quality", "fwhm", "z"],
                 [int, float, float, float, float, float, float, float, float,
                     float, int],
                 ["%d", "%10.5f", "%10.5f", "%10.5f", formatSeconds, formatSeconds,
-                    formatSeconds, "%.2e", "%.2e", formatSeconds, "%d" ],
+                    formatSeconds, "%.2e", "%.2e", formatSeconds, "%d"],
                 rows)
 
     tab.addConstantColumn("peakmap", peak_map, PeakMap, None)

commit 63327a7de4b1bd43a30bf42a2de881c1d6f4f336

SSDM-2511 : fixed typo in feature_detector_batches.py introduced in commit 1aa69ea9

emzed/batches/peak_picker.py
@@ -75,7 +75,7 @@ def runPeakPickerHiRes(pattern=None, destination=None, configid=None, **params):
 
         def write(self, result, destinationDir, path):
             basename, ext = os.path.splitext(os.path.basename(path))
-            savePath = os.path.join(destinationDir, basename * "_centroided.mzML")
+            savePath = os.path.join(destinationDir, basename + "_centroided.mzML")
             print "save to ", savePath
             io.storePeakMap(result, savePath)
 