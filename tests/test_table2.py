uweschmitt/emzed2

commit 04e6303e601983d4242cc9fb9ecb5c8b5a009b20

SSDM-2511 : implemented multi sort with support for different order per given column name

emzed/core/data_types/table.py
@@ -818,20 +818,36 @@ class Table(object):
         if isinstance(colNames, basestring):
             colNames = [colNames]
 
+        if ascending in (True, False):
+            ascending = [ascending] * len(colNames)
+
+        assert len(colNames) == len(ascending)
+
         idxs = [self.colIndizes[name] for name in colNames]
 
-        decorated = [([row[idx] for idx in idxs], i)
-                     for (i, row) in enumerate(self.rows)]
-        decorated.sort(reverse=not ascending)
-        permutation = [i for (_, i) in decorated]
+        def compare(item1, item2):
+            __, row1 = item1
+            __, row2 = item2
+            for i, ai in zip(idxs, ascending):
+                v1 = row1[i]
+                v2 = row2[i]
+                if v1 != v2:
+                    if ai:
+                        return -1 if v1 < v2 else +1
+                    else:
+                        return -1 if v1 > v2 else +1
+            return 0
 
-        self._applyRowPermutation(permutation)
+        decorated = list(enumerate(self.rows))
+        decorated.sort(cmp=compare)
+        perm, __ = zip(*decorated)
+        self._applyRowPermutation(perm)
 
         if ascending:
             self.primaryIndex = {colNames[0]: True}
         else:
             self.primaryIndex = {}
-        return permutation
+        return perm
 
     def _applyRowPermutation(self, permutation):
         self.rows = [self.rows[permutation[i]]

tests/test_table2.py
@@ -588,17 +588,60 @@ def test_replace_dunder_coloumn():
     t.print_()
 
 
-def test_multi_sort():
-    t = emzed.utils.toTable("z", [1, 3, 2])
-    t.addColumn("y", [1, 2, 2])
+def test_multi_sort(regtest):
+    t = emzed.utils.toTable("x", [1, 2, 2, 4])
+    t.addColumn("y", [1, 1, 2, 2])
+    t.addColumn("z", [1, 1, 1, 2])
 
+    print(t, file=regtest)
+
+    print("x then y", file=regtest)
+    t.sortBy(["x", "y"])
+    print(t, file=regtest)
+
+    print("x then z", file=regtest)
+    t.sortBy(["x", "z"])
+    print(t, file=regtest)
+
+    print("y then z", file=regtest)
     t.sortBy(["y", "z"])
-    assert t.y.values == (1, 2, 2,)
-    assert t.z.values == (1, 2, 3,)
+    print(t, file=regtest)
 
-    t.sortBy(["y", "z"], ascending=False)
-    assert t.y.values == (2, 2, 1,)
-    assert t.z.values == (3, 2, 1,)
+    print("y then x", file=regtest)
+    t.sortBy(["y", "x"])
+    print(t, file=regtest)
+
+    print("z then x", file=regtest)
+    t.sortBy(["z", "x"])
+    print(t, file=regtest)
+
+    print("z then y", file=regtest)
+    t.sortBy(["z", "y"])
+    print(t, file=regtest)
+
+    print("x then y (desc)", file=regtest)
+    t.sortBy(["x", "y"], (True, False))
+    print(t, file=regtest)
+
+    print("x then z (desc)", file=regtest)
+    t.sortBy(["x", "z"], (True, False))
+    print(t, file=regtest)
+
+    print("y then z (desc)", file=regtest)
+    t.sortBy(["y", "z"], (True, False))
+    print(t, file=regtest)
+
+    print("y then x (desc)", file=regtest)
+    t.sortBy(["y", "x"], (True, False))
+    print(t, file=regtest)
+
+    print("z then x (desc)", file=regtest)
+    t.sortBy(["z", "x"], (True, False))
+    print(t, file=regtest)
+
+    print("z then y (desc)", file=regtest)
+    t.sortBy(["z", "y"], (True, False))
+    print(t, file=regtest)
 
 
 def test_collapse(regtest):

commit 1205aecb797609bacd6da23654b3f34ef3b7c828

SSDM-2511 : fixed regression from 04e6303e601 if yon sorts an empty table

emzed/core/data_types/table.py
@@ -823,6 +823,9 @@ class Table(object):
 
         assert len(colNames) == len(ascending)
 
+        if not len(self):
+            return []   # empty permutation
+
         idxs = [self.colIndizes[name] for name in colNames]
 
         def compare(item1, item2):

tests/test_table2.py
@@ -581,11 +581,16 @@ def test_drop_last_column():
 
 
 def test_replace_dunder_coloumn():
-    t = emzed.utils.toTable("z", [1, 2])
+    t = emzed.utils.toTable("z", [1, 2], type_=int)
     t = t.join(t, True)
     # should not throw exception, as we do not create a new columns with "__" in its name:
     t.replaceColumn("z__0", t.z__0.apply(float))
     t.print_()
+
+def test_sort_empty_table():
+    t = emzed.utils.toTable("z", (), type_=int)
+    print(t)
+    t.sortBy("z")
 
 
 def test_multi_sort(regtest):