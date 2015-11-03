92bc0df59505c9af8e4b94bda58af1dd0fe3d11c
uweschmitt <uwe.schmitt@id.ethz.ch>
2015-11-03 17:35:16 +0100

    SSDM-2641 : release 2.22.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 # no import emzed here, causes trouble when installing on win, as missing packages
 # are needed when importing emzed
-version = (2, 22, 0)
+version = (2, 22, 1)
 
 
 install_requires = ["emzed_optimizations",
