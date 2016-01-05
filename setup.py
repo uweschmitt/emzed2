82bd074fcca3b57882c7a6568bc574ef6444a64a
uweschmitt <uwe.schmitt@id.ethz.ch>
2016-01-05 15:42:02 +0100

    release 2.23.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 # no import emzed here, causes trouble when installing on win, as missing packages
 # are needed when importing emzed
-version = (2, 23, 0)
+version = (2, 23, 1)
 
 
 install_requires = ["emzed_optimizations>=0.5.0",
