uweschmitt/emzed2

commit 0d97b6aa32dec57a44bf3e35e4988882bcc99577

SSDM-2932 : fixed issue with nan values among times in TimeSeries

emzed/core/data_types/col_types.py
@@ -1,6 +1,8 @@
 import hashlib
 import cPickle
 
+import numpy as np
+
 
 def unique_id_from(*args):
     h = hashlib.sha256()
@@ -76,7 +78,7 @@ class TimeSeries(object):
         this generate yields the pairs (xi, yi) for every segment.
         """
         segments = []
-        ni = [-1] + [i for i, xi in enumerate(x) if xi is None] + [len(x)]
+        ni = [-1] + [i for i, xi in enumerate(x) if xi is None or xi is np.nan] + [len(x)]
         for (si, ti) in zip(ni, ni[1:]):
             if si + 1 < ti:
                 s = slice(si + 1, ti)

commit da28553aac43e5e05dd4d7a9a8c6ad49fc982460

SSDM-2932 : fixed regression from commit 0d97b6aa32

emzed/core/data_types/col_types.py
@@ -78,7 +78,9 @@ class TimeSeries(object):
         this generate yields the pairs (xi, yi) for every segment.
         """
         segments = []
-        ni = [-1] + [i for i, xi in enumerate(x) if xi is None or xi is np.nan] + [len(x)]
+        def invalid(x, y):
+            return x is None or y is None or x is np.nan or y is np.nan
+        ni = [-1] + [i for i, (xi, yi) in enumerate(zip(x, y)) if invalid(xi, yi)] + [len(x)]
         for (si, ti) in zip(ni, ni[1:]):
             if si + 1 < ti:
                 s = slice(si + 1, ti)