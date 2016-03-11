158a8f0ae6231b1c53fd05c6cd919f51e96fa7cd
uweschmitt <uwe.schmitt@id.ethz.ch>
2016-03-11 11:11:21 +0100

    fixed install.bat which was unstable on euler

--- windows_installer/install_emzed.bat
+++ windows_installer/install_emzed.bat
@@ -15,6 +15,13 @@ call %INSTALLTARGET%\Scripts\activate
 @echo.
 pip install -U setuptools
 @echo.
+
+rem "pip install" will download a binary package if available, but when
+rem resolving dependencies pip will download source packages. This causes
+rem trouble on most machines having no appropriate microsoft compiler
+rem installed.  so we first install all pre compiled binary packages and the
+rem final "pip install emzed" will only install source distributed stuff:
+
 pip install pyopenms
 @echo.
 python -c "import pyopenms"
@@ -25,11 +32,9 @@ pip install -U ipython==0.10
 @echo.
 pip install -U dill
 @echo.
-set MPLCONFIGDIR=.
-
 pip install "pycryptodome<=3.3"
-
-easy_install emzed
+@echo.
+pip install emzed
 
 REM sometimes matplotlib setup is broken, this should fix this:
 set MPLCONFIGDIR=%APPDATA%\matplotlib_config
@@ -38,4 +43,3 @@ python -c "import matplotlib"
 
 REM boostrap libs:
 emzed.workbench.debug
-
