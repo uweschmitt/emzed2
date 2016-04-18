22e3f47c21e2de4019df261703123441398fdf98
uweschmitt <uwe.schmitt@id.ethz.ch>
2016-04-18 13:13:56 +0200

    SSDM-3532 : release 2.26.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 # no import emzed here, causes trouble when installing on win, as missing packages
 # are needed when importing emzed
-version = (2, 26, 2)
+version = (2, 26, 3)
 
 
 install_requires = [
