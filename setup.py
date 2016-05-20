62742a05c25c63942ad46553d8b106749a0da56a
uweschmitt <uwe.schmitt@id.ethz.ch>
2016-05-20 15:22:57 +0200

    SSDM-3621 : release 2.26.18

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 # no import emzed here, causes trouble when installing on win, as missing packages
 # are needed when importing emzed
-version = (2, 26, 17)
+version = (2, 26, 18)
 
 
 install_requires = ["guidata<=1.6.2",
