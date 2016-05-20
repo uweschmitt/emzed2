3557ae67a18ccdf2eba16e39d3ea8693e07cd2da
uweschmitt <uwe.schmitt@id.ethz.ch>
2016-05-20 14:43:02 +0200

    SSDM-3621 : added module for simpler import of often used data type + added hdf5 stuff to emzed.io

---
+++ emzed/data_types.py
@@ -0,0 +1,4 @@
+# encoding: utf-8
+from __future__ import print_function, division, absolute_import
+
+from emzed.core.data_types import PeakMap, Table, Spectrum, TimeSeries, Blob, CheckState
--- emzed/io/__init__.py
+++ emzed/io/__init__.py
@@ -1,4 +1,6 @@
 
+from ..core.data_types import to_hdf5, append_to_hdf5, atomic_hdf5_writer, Hdf5TableProxy
+
 from load_utils import loadCSV, loadTable, loadPeakMap, loadBlob
 from store_utils import storeCSV, storeTable, storePeakMap, storeBlob
 
