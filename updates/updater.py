8d3e86f5109a0412c314f1da574042800df9a0dd
uweschmitt <uwe.schmitt@id.ethz.ch>
2016-05-25 11:15:38 +0200

    SSDM-3621 : updated updater.py for recent changes

--- updates/updater.py
+++ updates/updater.py
@@ -2,22 +2,39 @@
 from __future__ import print_function
 
 def version():
-    return (2, 24, 6)
+    return (2, 26, 19)
 
 
 def description():
     msg = """
-    release 2.24.6:
-    this release provides some bugfixes and minor performance improvements.
+    release 2.26.19:
+       - hdf5 based table stores
+       - performance improvements
+       - atomic writes
+       - more responsive table explorer for large files and hdf5 files
+       - small bug fixes
     """
     return msg
 
 
 def run_update(locally=True):
     import pip
-    pip.main("install -U pycryptodome<=3.3".split())
-    pip.main("install -U emzed_optimizations>=0.5.0".split())
-    pip.main("install emzed==2.24.6".split())
+                    "pycryptodome<=3.3",
+                    "xlwt",
+                    "xlrd",
+                    "openpyxl",
+
+    pip.main("install pycryptodome<=3.3".split())
+    pip.main("install emzed_optimizations>=0.6.0".split())
+
+    pip.main("install xlwt".split())
+    pip.main("install xlrd".split())
+
+    pip.main("install jdcal".split())
+    pip.main("install et-xmlfile".split())
+    pip.main("install openpyxl".split())
+
+    pip.main("install emzed==2.26.19".split())
 
 
 if __name__ == "__main__":
