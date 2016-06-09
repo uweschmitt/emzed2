45e05c4492c393516157e73f3616a94ac7f43bb7
uweschmitt <uwe.schmitt@id.ethz.ch>
2016-06-09 16:59:32 +0200

    release 2.27.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 # no import emzed here, causes trouble when installing on win, as missing packages
 # are needed when importing emzed
-version = (2, 26, 19)
+version = (2, 27, 0)
 
 
 install_requires = ["guidata<=1.6.2",
