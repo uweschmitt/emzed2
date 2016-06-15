4172d70027b90b87ecba7d737ba45307fc4aab69
uweschmitt <uwe.schmitt@id.ethz.ch>
2016-06-15 22:10:21 +0200

    added missing colors.py module

---
+++ emzed/core/explorers/colors.py
@@ -0,0 +1,14 @@
+
+def getColors(i, light=False):
+    colors = [(0, 0, 200), (70, 70, 70), (0, 150, 0), (200, 0, 0), (200, 200, 0), (100, 70, 0)]
+    c = colors[i % len(colors)]
+    color = "#" + "".join("%02x" % v for v in c)
+    if light:
+        color = turn_light(color)
+    return color
+
+
+def turn_light(color):
+    rgb = [int(color[i:i + 2], 16) for i in range(1, 6, 2)]
+    rgb_light = [min(ii + 50, 255) for ii in rgb]
+    return "#" + "".join("%02x" % v for v in rgb_light)
