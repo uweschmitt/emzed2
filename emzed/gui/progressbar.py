2a194fe702392262f5c0949cbd09c16f56ebbea6
uweschmitt <uwe.schmitt@id.ethz.ch>
2016-06-22 11:46:45 +0200

    improved emzed.gui.ProgressBar

--- emzed/gui/progressbar.py
+++ emzed/gui/progressbar.py
@@ -5,10 +5,7 @@ from contextlib import contextmanager
 import time
 
 import guidata
-from PyQt4.QtCore import QThread
 from PyQt4.QtGui import QProgressDialog
-
-
 
 
 @contextmanager
@@ -20,7 +17,6 @@ def ProgressBar(n_steps, label="", allow_cancel=False, parent=None):
             for i in range(100):
 
                 # we simulate work of step i
-                time.sleep(.05)
 
                 # we update progressbar
                 handler.update(i, "step %03d" % i)
@@ -33,6 +29,8 @@ def ProgressBar(n_steps, label="", allow_cancel=False, parent=None):
     app = guidata.qapplication()
     dlg = QProgressDialog(parent)
     dlg.setLabelText(label)
+    dlg.setAutoClose(False)
+    dlg.setAutoReset(False)
     if allow_cancel:
         dlg.setCancelButtonText("Cancel")
     dlg.setMaximum(n_steps)
@@ -45,16 +43,19 @@ def ProgressBar(n_steps, label="", allow_cancel=False, parent=None):
             self._n = 0
             self._canceled = False
             dlg.canceled.connect(self._set_canceled)
+            dlg.setValue(0)
 
         def _set_canceled(self):
             self._canceled = True
             dlg.close()
 
         def update(self, n, message=None):
+            app.processEvents()
             self._n = n
-            dlg.setValue(n)
+            dlg.setValue(n + 1)
             if message is not None:
                 dlg.setLabelText(message)
+            dlg.update()
             app.processEvents()
 
         def is_canceled(self):
