uweschmitt/emzed2

commit 63b913525e8d868ecc2f30bad31fc7b07bafafc8

fixed issues in table slicing for out of bound indices

emzed/core/data_types/table.py
@@ -71,8 +71,6 @@ def atomic_open_for_write(path, flags):
     try_to_move(temp_path, path)
 
 
-
-
 def relative_path(from_, to):
     def split(p):
         result = []
@@ -596,6 +594,9 @@ class Table(MutableTable):
         else:
             irow = ix
 
+        def wraparound(i, n):
+            return i + n if i < 0 else i
+
         def _setup(ix, n):
             if ix is not None:
                 if isinstance(ix, np.ndarray):
@@ -606,6 +607,14 @@ class Table(MutableTable):
                     stepsize = ix.step if ix.step is not None else 1
                     if stepsize < 0:
                         start, end = end - 1, start - 1
+
+                    # handle negative indices:
+                    start = wraparound(start, n)
+                    end = wraparound(end, n)
+                    # capture "out of bounds" indexing:
+                    start = min(start, n)
+                    end = min(end, n)
+
                     ix = range(start, end, stepsize)
                 elif isinstance(ix, (int, long)):
                     ix = [ix]
@@ -615,17 +624,37 @@ class Table(MutableTable):
                 ix = range(0, n)
             return ix
 
-        icol = _setup(icol, len(self._colNames))
-        irow = _setup(irow, len(self))
+        nrows, ncols = self.shape
 
-        def select(li, ix):
-            return li[:] if ix is None else [li[i] for i in ix]
+        icol = _setup(icol, ncols)
+        irow = _setup(irow, nrows)
+
+        def select(li, ix, n, mode):
+            if ix is None:
+                return li[:]
+            for i in ix:
+                imod = wraparound(i, n)
+                if not 0 <= imod < n:
+                    raise IndexError("you tried to access %s with out of bounds index %d" % (mode, i))
+            return [li[i] for i in ix]
 
         prototype = self.buildEmptyClone(icol)
-        for row in select(self.rows, irow):
-            prototype.rows.append(select(row, icol))
+        for row in select(self.rows, irow, nrows, "row"):
+            prototype.rows.append(select(row, icol, ncols, "column"))
         prototype.resetInternals()
         return prototype
+
+    def __eq__(self, other):
+        if not isinstance(other, Table):
+            return False
+        return self._colNames == other._colNames and \
+            len(self) == len(other) and \
+            self._colTypes == other._colTypes and \
+            self._colFormats == other._colFormats and \
+            self.rows == other.rows
+
+    def __ne__(self, other):
+        return not (self == other)
 
     def overwrite(self, other):
         Table._check_if_compatible((self, other))
@@ -1629,7 +1658,9 @@ class Table(MutableTable):
         self._setupColumnAttributes()
         self._resetUniqueId()
 
-        self.shape = (len(self), len(self._colNames))
+    @property
+    def shape(self):
+        return len(self), len(self._colNames)
 
     def uniqueRows(self, byColumns=None):
         """

tests/test_table2.py
@@ -1535,3 +1535,34 @@ def test_append(regtest):
     t0 = emzed.utils.toTable("b", (0, 0, None), type_=int)
     t.appendTable(t0)
     print(t, file=regtest)
+
+
+def test_table_slicing():
+    t = emzed.utils.toTable("a", (1, 2, 3, 4), type_=int)
+    t.addColumn("b", t.a + 10, type_=int)
+
+    assert t[:] == t
+    assert len(t[0]) == 1
+    assert len(t[:1]) == 1
+    assert t[:1] == t[0]
+
+    assert len(t[0:2]) == 2
+    assert len(t[:2]) == 2
+    assert t[0:2] == t[:2]
+
+    assert len(t[2:]) == 2
+    assert len(t[-2:]) == 2
+    assert t[-2:] == t[2:]
+    assert t[-2:-1] == t[2:3]
+
+    assert t[-1:] != t[2:]
+    assert t[-2:-1] != t[2:5]
+
+    assert t[0:4] == t
+    assert t[0:5] == t
+    assert t[0:6] == t
+    assert t[0:16] == t
+    assert t[-4] == t[0]
+
+    assert len(t[2:5]) == len(t[2:])
+

emzed/core/explorers/table_explorer.py
@@ -141,7 +141,7 @@ class ButtonDelegate(QItemDelegate):
     A delegate that places a fully functioning QPushButton in every
     cell of the column to which it's applied
 
-    we have to distinguis view and parent here: using the view as parent does not work
+    we have to distinguish view and parent here: using the view as parent does not work
     in connection with modal dialogs opened in the click handler !
     """
 

commit 8f3b553c5b1f2625c7966e1642e7fcb823cf06b6

fixed regression from 63b91352

emzed/core/data_types/table.py
@@ -600,12 +600,14 @@ class Table(MutableTable):
                     start = ix.start if ix.start is not None else 0
                     end = ix.stop if ix.stop is not None else n
                     stepsize = ix.step if ix.step is not None else 1
-                    if stepsize < 0:
-                        start, end = end - 1, start - 1
 
                     # handle negative indices:
                     start = wraparound(start, n)
                     end = wraparound(end, n)
+
+                    if stepsize < 0:
+                        start, end = end - 1, start - 1
+
                     # capture "out of bounds" indexing:
                     start = min(start, n)
                     end = min(end, n)

tests/test_table2.py
@@ -1063,6 +1063,36 @@ def test_getitem_variations():
         assert ti.getColFormats() == t.getColFormats()[::-1]
 
 
+    t = emzed.utils.toTable("a", (1, 2, 3, 4), type_=int)
+    t.addColumn("b", t.a + 10, type_=int)
+
+    assert t[:] == t
+    assert len(t[0]) == 1
+    assert len(t[:1]) == 1
+    assert t[:1] == t[0]
+
+    assert len(t[0:2]) == 2
+    assert len(t[:2]) == 2
+    assert t[0:2] == t[:2]
+
+    assert len(t[2:]) == 2
+    assert len(t[-2:]) == 2
+    assert t[-2:] == t[2:]
+    assert t[-2:-1] == t[2:3]
+
+    assert t[-1:] != t[2:]
+    assert t[-2:-1] != t[2:5]
+
+    assert t[0:4] == t
+    assert t[0:5] == t
+    assert t[0:6] == t
+    assert t[0:16] == t
+    assert t[-4] == t[0]
+
+    assert len(t[2:5]) == len(t[2:])
+
+
+
 def test_t():
     t = emzed.utils.toTable("v", range(1, 3))
 
@@ -1535,34 +1565,3 @@ def test_append(regtest):
     t0 = emzed.utils.toTable("b", (0, 0, None), type_=int)
     t.appendTable(t0)
     print(t, file=regtest)
-
-
-def test_table_slicing():
-    t = emzed.utils.toTable("a", (1, 2, 3, 4), type_=int)
-    t.addColumn("b", t.a + 10, type_=int)
-
-    assert t[:] == t
-    assert len(t[0]) == 1
-    assert len(t[:1]) == 1
-    assert t[:1] == t[0]
-
-    assert len(t[0:2]) == 2
-    assert len(t[:2]) == 2
-    assert t[0:2] == t[:2]
-
-    assert len(t[2:]) == 2
-    assert len(t[-2:]) == 2
-    assert t[-2:] == t[2:]
-    assert t[-2:-1] == t[2:3]
-
-    assert t[-1:] != t[2:]
-    assert t[-2:-1] != t[2:5]
-
-    assert t[0:4] == t
-    assert t[0:5] == t
-    assert t[0:6] == t
-    assert t[0:16] == t
-    assert t[-4] == t[0]
-
-    assert len(t[2:5]) == len(t[2:])
-