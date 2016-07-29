9778a65c4a2fa2780a9dd6e5c0a44ee27b52b73b
uweschmitt <uwe.schmitt@id.ethz.ch>
2016-07-29 14:00:15 +0200

    handle 'postN' versions in versions.py as numbers

--- emzed/version.py
+++ emzed/version.py
@@ -2,6 +2,16 @@ import sys
 
 if not getattr(sys, "frozen", False):
     import pkg_resources
-    version = tuple(map(str, pkg_resources.require("emzed")[0].version.split(".")))
+    version = pkg_resources.require("emzed")[0].version.split(".")
+    if version[-1].startswith("post"):
+        version[-1] = version[-1][4:]
+    for i, vi in enumerate(version):
+        try:
+            vi = int(vi)
+        except ValueError:
+            pass
+        version[i] = vi
+    version = tuple(version)
+
 else:
     version = (0, 0, 0)
