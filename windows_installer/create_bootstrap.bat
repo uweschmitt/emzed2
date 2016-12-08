86b4540ea2c46b3b5aa45eb5eb3f5b5904920f39
uweschmitt <uwe.schmitt@id.ethz.ch>
2016-12-08 20:15:25 +0100

    windows instaler now writes donwloaded file to %APPDATA%
        writing it next to the installed files may not work if
        permission to write is not granted.

--- windows_installer/create_bootstrap.bat
+++ windows_installer/create_bootstrap.bat
@@ -2,8 +2,8 @@ set PYTHONPATH=%1
 > run_or_bootstrap.bat (
     @echo set SCRIPTS=%%APPDATA%%\emzed2\Scripts
     @echo call %%SCRIPTS%%\activate ^|^| ^(
-    @echo     %PYTHONPATH% -c "import urllib; urllib.urlretrieve('http://emzed.ethz.ch/downloads/install_emzed.bat', 'install_emzed.bat')"
-    @echo     call install_emzed.bat %PYTHONPATH%
+    @echo     %PYTHONPATH% -c "import urllib, os; app=os.environ.get('APPDATA'); urllib.urlretrieve('http://emzed.ethz.ch/downloads/install_emzed.bat', os.path.join(app, 'install_emzed.bat'))"
+    @echo     call %%APPDATA%%\install_emzed.bat %PYTHONPATH%
     @echo ^)
     @echo call %%SCRIPTS%%\activate
     @echo call %%SCRIPTS%%\emzed.workbench.exe
